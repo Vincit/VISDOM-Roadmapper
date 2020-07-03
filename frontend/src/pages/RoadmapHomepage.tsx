c943676a35b47b4c6a25e781495cfd6dcd14d283
Aleksi Kytölä <aleksi.kytola@vincit.fi>
2020-07-03 17:15:41 +0300

    Removed roadmap selection from roadmap home page

--- frontend/src/pages/RoadmapHomepage.tsx
+++ frontend/src/pages/RoadmapHomepage.tsx
@@ -1,42 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Col } from 'react-bootstrap';
-import { shallowEqual, useDispatch, useSelector } from 'react-redux';
-import { LoadingSpinner } from '../components/LoadingSpinner';
-import { RoadmapSelectorWidget } from '../components/RoadmapSelectorWidget';
-import { StoreDispatchType } from '../redux';
-import { roadmapsActions } from '../redux/roadmaps';
-import { roadmapsSelector } from '../redux/roadmaps/selectors';
-import { Roadmap } from '../redux/roadmaps/types';
-import { RootState } from '../redux/types';
+import React from 'react';
 
 export const RoadmapHomePage = () => {
-  const dispatch = useDispatch<StoreDispatchType>();
-  const roadmaps = useSelector<RootState, Roadmap[] | undefined>(
-    roadmapsSelector,
-    shallowEqual,
-  );
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    dispatch(roadmapsActions.getRoadmaps()).then(() => setIsLoaded(true));
-  }, [dispatch]);
-
-  return (
-    <>
-      {isLoaded ? (
-        <>
-          {roadmaps && roadmaps.length > 0 ? (
-            <Col>
-              Please select a roadmap
-              <RoadmapSelectorWidget />
-            </Col>
-          ) : (
-            'No roadmaps available'
-          )}
-        </>
-      ) : (
-        <LoadingSpinner />
-      )}
-    </>
-  );
+  return <>Roadmap home page</>;
 };
