7979f536b0b284e574ee880ca9cd6301f4e47943
Aleksi Kytölä <aleksi.kytola@vincit.fi>
2020-08-12 11:05:11 +0300

    Remove redundant env files, add temporary display for task weighted priority values

--- frontend/src/components/TaskRatingsText.tsx
+++ frontend/src/components/TaskRatingsText.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { StarFill, Wrench } from 'react-bootstrap-icons';
-import { Task, TaskRatingDimension } from '../redux/roadmaps/types';
-import { calcTaskAverageRating } from '../utils/TaskUtils';
+import { shallowEqual, useSelector } from 'react-redux';
+import { publicUsersSelector } from '../redux/roadmaps/selectors';
+import { PublicUser, Task, TaskRatingDimension } from '../redux/roadmaps/types';
+import { RootState } from '../redux/types';
+import {
+  calcTaskAverageRating,
+  calcWeightedTaskPriority,
+} from '../utils/TaskUtils';
 
 export const TaskRatingsText: React.FC<{ task: Task }> = ({ task }) => {
   const averageBusinessVal = calcTaskAverageRating(
@@ -11,6 +17,10 @@ export const TaskRatingsText: React.FC<{ task: Task }> = ({ task }) => {
   const averageWorkVal = calcTaskAverageRating(
     TaskRatingDimension.RequiredWork,
     task,
+  );
+  const publicUsers = useSelector<RootState, PublicUser[] | undefined>(
+    publicUsersSelector,
+    shallowEqual,
   );
   return (
     <>
@@ -24,6 +34,11 @@ export const TaskRatingsText: React.FC<{ task: Task }> = ({ task }) => {
         <span className="mr-1 font-weight-bold">
           {averageWorkVal}
           <Wrench />
+        </span>
+      )}
+      {averageWorkVal && averageBusinessVal && (
+        <span className="mr-1 font-weight-bold">
+          {calcWeightedTaskPriority(task, publicUsers!)}
         </span>
       )}
       {!averageWorkVal && !averageBusinessVal && <span>-</span>}
