2cef01218bfd8cd8d2f56039618ed56dc3bc6778
Aleksi Kytölä <aleksi.kytola@vincit.fi>
2020-08-12 10:52:54 +0300

    Add unique name constraint to versions schema

---
+++ server/src/migrations/20200812104946_versionsUniqueName.ts
@@ -0,0 +1,9 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema.table('versions', (table) => {
+    table.unique(['roadmapId', 'name']);
+  });
+}
+
+export async function down(knex: Knex): Promise<any> {}
