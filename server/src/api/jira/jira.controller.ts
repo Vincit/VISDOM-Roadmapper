bfa92e17bbe3074af2279139b0b9db21bdd7f75d
Aleksi Kytölä <aleksi.kytola@vincit.fi>
2020-10-15 11:24:42 +0300

    Add route to import tasks from jira board

--- server/src/api/jira/jira.controller.ts
+++ server/src/api/jira/jira.controller.ts
@@ -1,5 +1,6 @@
 import { RouteHandlerFnc } from '../../types/customTypes';
 import { jiraApi } from '../../utils/jiraclient';
+import Task from '../tasks/tasks.model';
 
 export const getBoards: RouteHandlerFnc = async (ctx, _) => {
   const boards = await jiraApi.getAllBoards();
@@ -9,4 +10,39 @@ export const getBoards: RouteHandlerFnc = async (ctx, _) => {
       name: board.name,
     };
   });
+};
+
+export const importBoard: RouteHandlerFnc = async (ctx, _) => {
+  const { boardId, roadmapId, createdByUser } = ctx.request.body;
+  const boardissues = await jiraApi.getIssuesForBoard(boardId);
+  const issueIds = boardissues.issues.map((issue: any) => issue.id);
+  const issues = await Promise.all(
+    issueIds.map(async (id: any) => await jiraApi.findIssue(id)),
+  );
+  const importedTasks: Task[] = [];
+  for (let issue of issues) {
+    const convertedIssue = issue as any;
+    const task = {
+      name: convertedIssue.fields.summary,
+      description: convertedIssue.fields.description || 'No description',
+      createdAt: convertedIssue.fields.created,
+      completed: false,
+      jiraId: parseInt(convertedIssue.id, 10),
+      createdByUser: createdByUser,
+      roadmapId: roadmapId,
+    };
+    const existing = await Task.query()
+      .where('jiraId', parseInt(convertedIssue.id, 10))
+      .first();
+    if (existing) {
+      const imported = await Task.query().patchAndFetchById(existing.id, task);
+      importedTasks.push(imported);
+    } else {
+      const imported = await Task.query().insert(task);
+      importedTasks.push(imported);
+    }
+  }
+
+  ctx.body = importedTasks.length + ' tasks imported';
+  ctx.status = 200;
 };
--- server/src/api/jira/jira.routes.ts
+++ server/src/api/jira/jira.routes.ts
@@ -1,8 +1,8 @@
 import KoaRouter from '@koa/router';
 import { Context, DefaultState } from 'koa';
-import { getBoards } from './jira.controller';
+import { getBoards, importBoard } from './jira.controller';
 const jiraRouter = new KoaRouter<DefaultState, Context>();
 
 jiraRouter.get('/jira/boards', getBoards);
-
+jiraRouter.post('/jira/importboard', importBoard);
 export default jiraRouter;
