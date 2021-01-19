a38d2cd04d455353f01b0fb54a576cdb0401642a
Aleksi Kytölä <aleksi.kytola@vincit.fi>
2021-01-19 15:03:53 +0200

    Add env JIRA_PRIVATE_KEY for staging use

--- server/src/utils/jiraclient.ts
+++ server/src/utils/jiraclient.ts
@@ -1,6 +1,10 @@
 import fs from 'fs';
 import JiraApi from 'jira-client';
 
+const JIRA_PRIVATEKEY =
+  process.env.JIRA_PRIVATE_KEY ||
+  fs.readFileSync(process.env.JIRA_PRIVATE_KEY_FILE!, 'utf8');
+
 export const jiraApi = new JiraApi({
   protocol: 'https',
   host: process.env.JIRA_HOST!,
@@ -8,10 +12,7 @@ export const jiraApi = new JiraApi({
   strictSSL: true,
   oauth: {
     consumer_key: 'roadmapper',
-    consumer_secret: fs.readFileSync(
-      process.env.JIRA_PRIVATE_KEY_FILE!,
-      'utf8',
-    ),
+    consumer_secret: JIRA_PRIVATEKEY,
     access_token: process.env.JIRA_ACCESS_TOKEN!,
     access_token_secret: process.env.JIRA_ACCESS_TOKEN_SECRET!,
   },
