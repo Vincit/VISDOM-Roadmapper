02ad13514bf6ed102722ab52d0fbfa061aa12e93
Henri Bomström <henri.bomstrom@gmail.com>
2021-01-27 18:05:17 +0200

    Implemented OAuth access token retrieval.

--- server/src/utils/jiraauthentication.ts
+++ server/src/utils/jiraauthentication.ts
@@ -1,6 +1,13 @@
 import JiraClient from 'jira-connector';
-import { JiraOAuthURLResponse, JiraOAuthRequestTokenResponse } from '../types/jiraTypes';
+import {
+  JiraOAuthURLResponse,
+  JiraOAuthRequestTokenResponse,
+} from '../types/jiraTypes';
 import NodeRSA from 'node-rsa';
+import JiraConfiguration from '../api/jiraconfigurations/jiraconfigurations.model';
+import User from '../api/users/users.model';
+import JiraApi from 'jira-client';
+import Token from '../api/tokens/tokens.model';
 
 const PRIVATE_KEY_EXPORT_FORMAT = 'pkcs1';
 
@@ -11,7 +18,7 @@ const createJiraAuthorizationURL = async (
   const pkey = new NodeRSA();
   pkey.importKey(privatekey);
 
-  // TODO: Replace consumer key.
+  // TODO: Replace consumer key?
   return new Promise((resolve, reject) => {
     JiraClient.oauth_util.getAuthorizeURL(
       {
@@ -67,5 +74,80 @@ const swapJiraOAuthToken = async (
   });
 };
 
+// TODO: Figure out a better way to implement this.
+const getJiraAccessTokens = (
+  jiraconfig: JiraConfiguration,
+  user: User,
+): {
+  accessToken: string | undefined;
+  accessTokenSecret: string | undefined;
+} => {
+  const requestToken = user.tokens.find((token) => {
+    return (
+      token.provider === 'jira' &&
+      token.type === 'access_token' &&
+      token.instance === jiraconfig.url
+    );
+  });
+  const requestTokenSecret = user.tokens.find((token) => {
+    return (
+      token.provider === 'jira' &&
+      token.type === 'access_token_secret' &&
+      token.instance === jiraconfig.url
+    );
+  });
+  return {
+    accessToken: requestToken?.value,
+    accessTokenSecret: requestTokenSecret?.value,
+  };
+};
+
+const createClient = (
+  jiraconfig: JiraConfiguration,
+  user: User,
+): JiraApi => {
+  const {accessToken, accessTokenSecret} = getJiraAccessTokens(jiraconfig, user);
+
+  const pkey = new NodeRSA();
+  pkey.importKey(jiraconfig.privatekey);
+
+  return new JiraApi({
+    protocol: 'https',
+    host: jiraconfig.url,
+    apiVersion: '2',
+    strictSSL: true,
+    oauth: {
+      consumer_key: 'roadmapper',
+      consumer_secret: pkey.exportKey(PRIVATE_KEY_EXPORT_FORMAT),
+      access_token: accessToken || '',
+      access_token_secret: accessTokenSecret || '',
+    },
+  });
+};
+
+// TODO: interface, class or something else to represent the token contents.
+const upsertToken = async (newToken: {
+  provider: string;
+  instance: string;
+  type: string;
+  user: number;
+  value: string;
+}) => {
+  // TODO: Extract providers and token types somewhere to avoid magic numbers.
+  const existing = await Token.query()
+    .where('tokens.user', newToken.user)
+    .where('provider', newToken.provider)
+    .where('instance', newToken.instance)
+    .where('type', newToken.type)
+    .first();
+  if (existing) {
+    await Token.query().patchAndFetchById(existing.id, newToken);
+  } else {
+    await Token.query().insert(newToken);
+  }
+};
+
 export const authorizationURL = createJiraAuthorizationURL;
 export const swapOAuthToken = swapJiraOAuthToken;
+export const createJiraClient = createClient;
+export const insertOrUpdateToken = upsertToken;
