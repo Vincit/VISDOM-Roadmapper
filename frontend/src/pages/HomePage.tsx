15e46363ee5ba5f97d24d1f6289d6661470f0d70
Juho Eerola <juho.eerola@vincit.fi>
2021-03-10 13:45:25 +0200

    Add landing page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { shallowEqual, useSelector } from 'react-redux';
+import { RootState } from '../redux/types';
+import { userInfoSelector } from '../redux/user/selectors';
+import { UserInfo } from '../redux/user/types';
+import { LandingPage } from './LandingPage';
 
 export const HomePage = () => {
-  return <div>This is the home page.</div>;
+  const loggedInUser = useSelector<RootState, UserInfo | undefined>(
+    userInfoSelector,
+    shallowEqual,
+  );
+  return loggedInUser ? <div>This is the home page.</div> : <LandingPage />;
 };
