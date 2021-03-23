94b4f6ab0598ba28994ffa3d7d3552dfa7c136d1
Aleksi Kytölä <aleksi.kytola@vincit.fi>
2021-03-23 15:32:47 +0200

    Remove unused app.module.scss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,14 +4,8 @@ import React from 'react';
 import { initReactI18next } from 'react-i18next';
 import { BrowserRouter } from 'react-router-dom';
 import { ModalRoot } from './components/modals/ModalRoot';
-import { NavBar } from './components/NavBar';
 import { i18config } from './i18/config';
 import { MainRouter } from './routers/MainRouter';
-import { RoadmapSidebar } from './components/RoadmapSidebar';
-import classNames from 'classnames';
-import css from './App.module.scss';
-
-const classes = classNames.bind(css);
 
 i18n.use(initReactI18next).init(i18config);
 export const App = () => {
