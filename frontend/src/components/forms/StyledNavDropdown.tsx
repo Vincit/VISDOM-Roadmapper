84f3c0fee5398555ca7d4ddf53d9650b8011431c
Patric Kangasmäki <patric.kangasmaki@vincit.fi>
2021-03-24 15:47:29 +0200

    Navbar style rework

--- frontend/src/components/forms/StyledNavDropdown.tsx
+++ frontend/src/components/forms/StyledNavDropdown.tsx
@@ -2,11 +2,18 @@ import { NavDropdown } from 'react-bootstrap';
 import styled from 'styled-components';
 
 export const StyledNavDropdown = styled(NavDropdown)`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 0px;
   border-radius: 16px;
+  border: 2px solid #0ec679;
+  box-sizing: border-box;
   height: 32px;
   min-width: 12em;
-  background-color: black;
-  font-size: 14px;
+  background-color: white;
+  font-size: 12px;
   line-height: 20px;
   margin-top: auto;
   margin-bottom: auto;
@@ -16,14 +23,14 @@ export const StyledNavDropdown = styled(NavDropdown)`
   }
 
   .dropdown-toggle {
-    color: white !important;
+    color: black !important;
     padding: 7px 0px 10px;
   }
 
   a {
     width: 100%;
-    font-size: 14px !important;
-    line-height: 20px !important;
+    font-size: 12px !important;
+    line-height: 18px !important;
     text-decoration: none;
     color: black;
     :hover {
