6eca5438c8ce9db2bf5e3092887ba5bdcf231110
Iida Kainu <iida.kainu@vincit.fi>
2021-03-29 12:26:30 +0300

    Rewriting plannerPageNavbar so conditional classnames are objects

--- frontend/src/components/PlannerPageNavbar.tsx
+++ frontend/src/components/PlannerPageNavbar.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Trans } from 'react-i18next';
 import { Link, useLocation, useRouteMatch } from 'react-router-dom';
-import { paths } from '../routers/paths';
 import classNames from 'classnames';
+import { paths } from '../routers/paths';
 import css from './PlannerPageNavbar.module.scss';
 
 const classes = classNames.bind(css);
@@ -13,38 +13,46 @@ export const PlannerPageNavbar = () => {
   return (
     <div className={classes(css.navbar)}>
       <Link
-        className={classes(css.navbarButton, 
-          pathname.startsWith(url + paths.plannerRelative.graph)
-          ? css.highlight : '')}
+        className={classes(css.navbarButton, {
+          [css.highlight]: !!pathname.startsWith(
+            url + paths.plannerRelative.graph,
+          ),
+        })}
         to={url + paths.plannerRelative.graph}
       >
         <Trans i18nKey="Roadmap" />
       </Link>
       <Link
-        className={classes(css.navbarButton, 
-          pathname.startsWith(url + paths.plannerRelative.editor)
-          ? css.highlight : '')}
+        className={classes(css.navbarButton, {
+          [css.highlight]: !!pathname.startsWith(
+            url + paths.plannerRelative.editor,
+          ),
+        })}
         to={url + paths.plannerRelative.editor}
       >
         <Trans i18nKey="Milestones" />
       </Link>
       <Link
-        className={classes(css.navbarButton, 
-          pathname.startsWith(url + paths.plannerRelative.timeEstimation)
-          ? css.highlight : '')}
+        className={classes(css.navbarButton, {
+          [css.highlight]: !!pathname.startsWith(
+            url + paths.plannerRelative.timeEstimation,
+          ),
+        })}
         to={url + paths.plannerRelative.timeEstimation}
       >
         <Trans i18nKey="Time Estimation" />
       </Link>
       <Link
-        className={classes(css.navbarButton, 
-          pathname.startsWith(url + paths.plannerRelative.weights)
-          ? css.highlight : '')}
+        className={classes(css.navbarButton, {
+          [css.highlight]: !!pathname.startsWith(
+            url + paths.plannerRelative.weights,
+          ),
+        })}
         to={url + paths.plannerRelative.weights}
       >
         <Trans i18nKey="Client Weights" />
       </Link>
-      <div className={classes(css.navbarFiller)}/>
+      <div className={classes(css.navbarFiller)} />
     </div>
   );
 };
