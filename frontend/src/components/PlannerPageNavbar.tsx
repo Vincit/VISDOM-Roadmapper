ae296ed213d71f36998d01557dce99feede62292
Iida Kainu <iida.kainu@vincit.fi>
2021-03-29 10:23:57 +0300

    Rework plannerPageNavbar without styled-components

--- frontend/src/components/PlannerPageNavbar.tsx
+++ frontend/src/components/PlannerPageNavbar.tsx
@@ -1,81 +1,50 @@
 import React from 'react';
 import { Trans } from 'react-i18next';
-import styled from 'styled-components';
 import { Link, useLocation, useRouteMatch } from 'react-router-dom';
 import { paths } from '../routers/paths';
+import classNames from 'classnames';
+import css from './PlannerPageNavbar.module.scss';
 
-const Navbar = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-`;
-
-const NavbarButton = styled(Link)<{ highlight: number }>`
-  font-size: 18px;
-  padding: 8px;
-  padding-left: 32px;
-  padding-right: 32px;
-  border-bottom: ${(props) =>
-    props.highlight ? '3px solid green' : '1px solid rgba(0, 0, 0, 0.3)'};
-  justify-content: center;
-  color: black;
-  text-align: center;
-  text-decoration: ${(props) => (props.highlight ? 'underline' : 'none')};
-  user-select: none;
-  background-color: ${(props) => (props.highlight ? '#e3e3e3' : 'inherit')};
-  :hover {
-    background-color: ${(props) => (props.highlight ? '#e3e3e3' : '#ededed')};
-    text-decoration: underline;
-    color: black;
-  }
-`;
-
-const NavbarFiller = styled.div`
-  display: flex;
-  flex-grow: 1;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.3);
-`;
+const classes = classNames.bind(css);
 
 export const PlannerPageNavbar = () => {
   const { url } = useRouteMatch();
   const { pathname } = useLocation();
   return (
-    <Navbar>
-      <NavbarButton
+    <div className={classes(css.navbar)}>
+      <Link
+        className={classes(css.navbarButton, 
+          pathname.startsWith(url + paths.plannerRelative.graph)
+          ? css.highlight : '')}
         to={url + paths.plannerRelative.graph}
-        highlight={
-          pathname.startsWith(url + paths.plannerRelative.graph) ? 1 : 0
-        }
       >
         <Trans i18nKey="Roadmap" />
-      </NavbarButton>
-      <NavbarButton
+      </Link>
+      <Link
+        className={classes(css.navbarButton, 
+          pathname.startsWith(url + paths.plannerRelative.editor)
+          ? css.highlight : '')}
         to={url + paths.plannerRelative.editor}
-        highlight={
-          pathname.startsWith(url + paths.plannerRelative.editor) ? 1 : 0
-        }
       >
         <Trans i18nKey="Milestones" />
-      </NavbarButton>
-      <NavbarButton
-        to={url + paths.plannerRelative.timeEstimation}
-        highlight={
+      </Link>
+      <Link
+        className={classes(css.navbarButton, 
           pathname.startsWith(url + paths.plannerRelative.timeEstimation)
-            ? 1
-            : 0
-        }
+          ? css.highlight : '')}
+        to={url + paths.plannerRelative.timeEstimation}
       >
         <Trans i18nKey="Time Estimation" />
-      </NavbarButton>
-      <NavbarButton
+      </Link>
+      <Link
+        className={classes(css.navbarButton, 
+          pathname.startsWith(url + paths.plannerRelative.weights)
+          ? css.highlight : '')}
         to={url + paths.plannerRelative.weights}
-        highlight={
-          pathname.startsWith(url + paths.plannerRelative.weights) ? 1 : 0
-        }
       >
         <Trans i18nKey="Client Weights" />
-      </NavbarButton>
-      <NavbarFiller />
-    </Navbar>
+      </Link>
+      <div className={classes(css.navbarFiller)}/>
+    </div>
   );
 };
