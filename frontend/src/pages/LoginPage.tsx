e21099b12862c920084b0fd0c910aa04706c249a
Iida Kainu <iida.kainu@vincit.fi>
2021-04-27 14:11:16 +0300

    Add a footer to loginPage

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -14,6 +14,7 @@ import { userInfoSelector } from '../redux/user/selectors';
 import { UserInfo } from '../redux/user/types';
 import { paths } from '../routers/paths';
 import css from './LoginPage.module.scss';
+import { Footer } from '../components/Footer';
 
 const classes = classNames.bind(css);
 
@@ -115,6 +116,7 @@ export const LoginPage = () => {
           </form>
         </ModalContent>
       </div>
+      <Footer />
     </>
   );
 };
