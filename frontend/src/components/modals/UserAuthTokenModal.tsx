d506836ddd51db05b979f4d7a36ba9871ac6b133
Juho Eerola <juho.eerola@vincit.fi>
2021-04-28 16:40:44 +0300

    Initial front end functionality for token management

--- frontend/src/components/modals/UserAuthTokenModal.tsx
+++ frontend/src/components/modals/UserAuthTokenModal.tsx
@@ -7,11 +7,40 @@ import { ModalContent } from './modalparts/ModalContent';
 import { ModalFooter } from './modalparts/ModalFooter';
 import { ModalFooterButtonDiv } from './modalparts/ModalFooterButtonDiv';
 import { ModalHeader } from './modalparts/ModalHeader';
+import { api } from '../../api/api';
 import '../../shared.scss';
 
 export const UserAuthTokenModal: React.FC<ModalProps> = ({ closeModal }) => {
   const [errorMessage, setErrorMessage] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [token, setToken] = useState('');
+  const [visible, setVisible] = useState(false);
+
+  function handleEvent<T>(endpoint: () => Promise<T>, callback: (_: T) => any) {
+    return (event: React.MouseEvent) => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      setIsLoading(true);
+      endpoint()
+        .then(callback)
+        .catch((reason) => {
+          setErrorMessage(reason.toString());
+        })
+        .finally(() => setIsLoading(false));
+    };
+  }
+
+  const newToken = handleEvent(api.generateCurrentUserToken, setToken);
+  const deleteToken = handleEvent(api.deleteCurrentUserToken, () => {
+    setVisible(true);
+    setToken('');
+  });
+  const showToken = handleEvent(api.getCurrentUserToken, (tok) => {
+    setVisible(true);
+    setToken(tok);
+  });
+  const toggleShow = () => setVisible((current) => !current);
 
   return (
     <>
@@ -22,6 +51,35 @@ export const UserAuthTokenModal: React.FC<ModalProps> = ({ closeModal }) => {
         <ModalCloseButton onClick={closeModal} />
       </ModalHeader>
       <ModalContent>
+        <div className="layoutRow" style={{ margin: '30px 0px', gap: '10px' }}>
+          <button
+            disabled={isLoading}
+            className="button-large"
+            onClick={token ? toggleShow : showToken}
+            type="button"
+          >
+            <Trans i18nKey={token && visible ? 'Hide token' : 'Show token'} />
+          </button>
+          <button
+            disabled={isLoading}
+            className="button-large"
+            onClick={newToken}
+            type="button"
+          >
+            <Trans i18nKey="Generate new" />
+          </button>
+          <button
+            disabled={isLoading}
+            className="button-large"
+            onClick={deleteToken}
+            type="button"
+          >
+            <Trans i18nKey="Delete" />
+          </button>
+        </div>
+        <Alert variant="info">
+          {visible ? token || 'no token' : token.replace(/./g, '*')}
+        </Alert>
         <Alert
           show={errorMessage.length > 0}
           variant="danger"
