9e19ac67997e0f813b55d13051c4885827df576c
Juho Eerola <juho.eerola@vincit.fi>
2021-05-10 16:32:38 +0300

    Migration for customer model

---
+++ server/src/migrations/20210510160632_addCustomerModel.ts
@@ -0,0 +1,46 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema
+    .createTable('customer', (table) => {
+      table.increments('id').primary();
+      table.string('name', 75).notNullable();
+      table.string('color', 7).nullable();
+      table.integer('value').unsigned().defaultTo(0);
+    })
+    .createTable('customerRepresentative', (table) => {
+      table
+        .integer('userId')
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE');
+      table
+        .integer('customerId')
+        .references('id')
+        .inTable('customer')
+        .onDelete('CASCADE');
+      table.primary(['userId', 'customerId']);
+    })
+    .alterTable('users', (table) => {
+      table.dropColumn('customerValue');
+    })
+    .alterTable('taskratings', (table) => {
+      table
+        .integer('forCustomer')
+        .references('id')
+        .inTable('customer')
+        .nullable();
+    });
+}
+
+export async function down(knex: Knex): Promise<any> {
+  return knex.schema
+    .alterTable('taskratings', (table) => {
+      table.dropColumn('forCustomer');
+    })
+    .alterTable('users', (table) => {
+      table.integer('customerValue').unsigned();
+    })
+    .dropTableIfExists('customerRepresentative')
+    .dropTableIfExists('customer');
+}
