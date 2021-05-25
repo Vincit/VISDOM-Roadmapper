eb649223b299819744189c6f29d0b47f9d87ed0b
Iida Kainu <iida.kainu@vincit.fi>
2021-05-25 14:23:09 +0300

    Remove responsive navbar
    Navbar will stay at the start of the page, regardless of user input

--- frontend/src/components/LoginNavBar.tsx
+++ frontend/src/components/LoginNavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import { Trans } from 'react-i18next';
 import { Link } from 'react-router-dom';
@@ -11,60 +11,28 @@ const classes = classNames.bind(css);
 
 export const LoginNavBar: React.FC<{
   type: 'landing' | 'login' | 'register';
-}> = ({ type }) => {
-  const [hidden, setHidden] = useState(false);
-  const [hiddenPos, setHiddenPos] = useState(0);
-  const [previousScroll, setPreviousScroll] = useState(0);
-
-  const handleScroll = () => {
-    const scroll = Math.round(window.pageYOffset);
-    if (scroll < previousScroll) {
-      setHidden(false);
-      setHiddenPos(scroll);
-    }
-    if (scroll - hiddenPos >= 80) {
-      setHidden(true);
-      setHiddenPos(scroll);
-    }
-    setPreviousScroll(scroll);
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll, true);
-    return () => window.removeEventListener('scroll', handleScroll, true);
-  });
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    });
-  };
-
-  return (
-    <div
-      className={classes(css.loginNavBar, {
-        [css.landingNavBar]: type === 'landing',
-        [css.hidden]: hidden,
-      })}
-    >
+}> = ({ type }) => (
+  <div
+    className={classes(css.loginNavBar, {
+      [css.landingNavBar]: type === 'landing',
+    })}
+  >
+    {type === 'landing' ? (
+      <VisdomLogo />
+    ) : (
       <Link to={paths.home} className={classes(css.visdomLogo)}>
-        <VisdomLogo onClick={scrollToTop} />
+        <VisdomLogo />
       </Link>
-      <span />
-      {type === 'login' ? (
-        <Link className={classes(css.loginButton)} to={paths.registerPage}>
-          <Trans i18nKey="Register" />
-        </Link>
-      ) : (
-        <Link className={classes(css.loginButton)} to={paths.loginPage}>
-          <Trans i18nKey="Login" />
-        </Link>
-      )}
-    </div>
-  );
-};
+    )}
+    <span />
+    {type === 'login' ? (
+      <Link className={classes(css.loginButton)} to={paths.registerPage}>
+        <Trans i18nKey="Register" />
+      </Link>
+    ) : (
+      <Link className={classes(css.loginButton)} to={paths.loginPage}>
+        <Trans i18nKey="Login" />
+      </Link>
+    )}
+  </div>
+);
--- frontend/src/components/NavLayout.tsx
+++ frontend/src/components/NavLayout.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
-import { shallowEqual, useSelector } from 'react-redux';
 import { NavBar } from './NavBar';
 import { RoadmapSidebar } from './RoadmapSidebar';
-import { RootState } from '../redux/types';
-import { userInfoSelector } from '../redux/user/selectors';
-import { UserInfo } from '../redux/user/types';
 import css from './NavLayout.module.scss';
 
 const classes = classNames.bind(css);
@@ -15,18 +11,16 @@ export interface NavLayoutProps {
 }
 
 export const NavLayout: React.FC<NavLayoutProps> = ({ Content }) => {
-  const userInfo = useSelector<RootState, UserInfo | undefined>(
-    userInfoSelector,
-    shallowEqual,
-  );
   return (
     <div className="layout-row grow">
       <RoadmapSidebar />
       <div
-        className={classes('layout-column', 'grow', css.appContent, {
-          'overflow-auto': userInfo,
-          [css.loggedIn]: userInfo,
-        })}
+        className={classes(
+          'layout-column',
+          'grow',
+          'overflow-auto',
+          css.appContent,
+        )}
       >
         <NavBar />
         <Content />
