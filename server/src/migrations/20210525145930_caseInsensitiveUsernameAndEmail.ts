932c1eedfda234b62a7cbb759d2c5c9fcd62a78e
Juho Eerola <juho.eerola@vincit.fi>
2021-05-25 15:49:04 +0300

    Require unique username and email compared in lowercase

---
+++ server/src/migrations/20210525145930_caseInsensitiveUsernameAndEmail.ts
@@ -0,0 +1,20 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema.alterTable('users', (table) => {
+    table.dropUnique(['username']);
+    table.dropUnique(['email']);
+  }).raw(`
+    CREATE UNIQUE INDEX lower_username ON users(LOWER(username));
+    CREATE UNIQUE INDEX lower_email ON users(LOWER(email));
+  `);
+}
+
+export async function down(knex: Knex): Promise<any> {
+  return knex.schema.alterTable('users', (table) => {
+    table.dropIndex([], 'lower_username');
+    table.dropIndex([], 'lower_email');
+    table.unique(['username']);
+    table.unique(['email']);
+  });
+}
