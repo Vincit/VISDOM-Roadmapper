9be84c18ce9d396c990e7cadfb91a49d0b00a896
Juho Eerola <juho.eerola@vincit.fi>
2021-05-26 16:31:36 +0300

    Remove dead code and unnecessary "eslint-disable"s

--- frontend/src/components/Debug.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Debug = ({
-  show = process.env.NODE_ENV === 'development',
-  children,
-}: {
-  show?: boolean;
-  children?: any;
-}) => (show && children ? <>{children}</> : null);
--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { Alert } from 'react-bootstrap';
