3f5bb768919a2992ccd3aeb105ce0f5a77d0dfb9
Juho Eerola <juho.eerola@vincit.fi>
2021-05-28 12:55:35 +0300

    Add customer to taskrating table unique constraint
    This allows the same user to rate tasks on behalf of multiple customers.

---
+++ server/src/migrations/20210528125152_addCustomerToTaskratingUniqueConstraint.ts
@@ -0,0 +1,20 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema.alterTable('taskratings', (table) => {
+    table.dropUnique(['parentTask', 'createdByUser', 'dimension']);
+    table.unique(['parentTask', 'createdByUser', 'forCustomer', 'dimension']);
+  });
+}
+
+export async function down(knex: Knex): Promise<any> {
+  return knex.schema.alterTable('taskratings', (table) => {
+    table.dropUnique([
+      'parentTask',
+      'createdByUser',
+      'forCustomer',
+      'dimension',
+    ]);
+    table.unique(['parentTask', 'createdByUser', 'dimension']);
+  });
+}
