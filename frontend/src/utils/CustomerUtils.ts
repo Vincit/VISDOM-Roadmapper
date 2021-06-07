a22e7bf0d61bc87d64051a7f54a9c35ee88b3fe2
Iida Kainu <iida.kainu@vincit.fi>
2021-06-07 10:09:23 +0300

    Add representatives to customer edition
    Representatives are users with RoleType of Admin or Business.

--- frontend/src/utils/CustomerUtils.ts
+++ frontend/src/utils/CustomerUtils.ts
@@ -1,3 +1,4 @@
+import convert from 'color-convert';
 import { Customer } from '../redux/roadmaps/types';
 
 import {
@@ -35,3 +36,15 @@ export const sortCustomers = (
   type: SortingTypes,
   order: SortingOrders,
 ) => sorted(customers, customerCompare(type), order);
+
+const difference = (first: number, second: number) =>
+  180 - Math.abs(Math.abs(first - second) - 180);
+
+export const randomColor = (customers: Customer[] | undefined): string => {
+  const hue = Math.random() * 360;
+  const found = customers?.find((obj) => {
+    if (obj.color) return difference(convert.hex.hsl(obj.color)[0], hue) < 30;
+  });
+  if (found) return randomColor(customers);
+  return `#${convert.hsl.hex([hue, 100, 65])}`;
+};
