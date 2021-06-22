5766f8f409b584a7178513e2e836d5e3bfb5a13a
Juho Eerola <juho.eerola@vincit.fi>
2021-06-22 12:01:38 +0300

    Add component for form fields
    The component wraps:
    - optional label
    - input element
    - error message for the input

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import assert from 'assert';
 import { withStyles } from '@material-ui/core/styles';
 import { Alert } from 'react-bootstrap';
 import classNames from 'classnames';
@@ -10,6 +11,7 @@ import { userActions } from '../redux/user';
 import { ModalContent } from '../components/modals/modalparts/ModalContent';
 import { ModalHeader } from '../components/modals/modalparts/ModalHeader';
 import { Footer } from '../components/Footer';
+import { Input, FieldProps, errorState } from '../components/forms/FormField';
 import { paths } from '../routers/paths';
 import { StoreDispatchType } from '../redux';
 import css from './RegisterPage.module.scss';
@@ -22,54 +24,113 @@ export const RegisterPage = () => {
   const history = useHistory();
   const { t } = useTranslation();
   const [formValues, setFormValues] = useState({
-    name: '',
+    username: '',
     email: '',
     password: '',
     confirmPassword: '',
   });
+
   const [checked, setChecked] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const errorHandler = (err: String, msg: String) => {
-    let field;
-    if (msg.includes('username')) field = t('username');
-    if (msg.includes('email')) field = t('email');
-
-    if (err === 'UniqueViolationError') return t('uniqueViolation', { field });
-    if (err === 'ValidationError') return t('validationError', { field });
-    return t('Internal server error.');
+  const fields: { [K in FormFieldName]: FieldProps<HTMLInputElement> } = {
+    username: {
+      label: t('Your name'),
+      placeholder: t('Your name'),
+      maxLength: 255,
+      error: errorState(useState('')),
+    },
+    email: {
+      label: t('Your email'),
+      placeholder: t('Example email'),
+      maxLength: 255,
+      autoComplete: 'off',
+      type: 'email',
+      error: errorState(useState('')),
+    },
+    password: {
+      label: t('Password'),
+      placeholder: '-',
+      type: 'password',
+      autoComplete: 'new-password',
+      minLength: 8,
+      maxLength: 72,
+      error: errorState(useState('')),
+    },
+    confirmPassword: {
+      label: t('Confirm password'),
+      placeholder: '-',
+      type: 'password',
+      autoComplete: 'confirm-password',
+      minLength: 8,
+      maxLength: 72,
+      error: errorState(useState('')),
+    },
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  type FormFieldName = keyof typeof formValues;
+
+  const knownField = (value: any): value is FormFieldName =>
+    value in formValues;
+
+  const setError = (column: string, message: string) => {
+    if (knownField(column)) {
+      fields[column].error!.setMessage(message);
+    } else {
+      setErrorMessage(message);
+    }
+  };
+
+  const errorHandler = (
+    type: string | undefined,
+    data?: {
+      message: string;
+      columns: string[] | { column: string; message: string }[];
+    },
+  ) => {
+    switch (type) {
+      case 'UniqueViolationError': {
+        const cols = data!.columns as string[];
+        cols.forEach((column) => {
+          setError(column, t('uniqueViolation', { field: t(column) }));
+        });
+        break;
+      }
+      case 'ValidationError': {
+        const cols = data!.columns as { column: string; message: string }[];
+        cols.forEach(({ column }) => {
+          setError(column, t('validationError', { field: t(column) }));
+        });
+        break;
+      }
+      default:
+        setErrorMessage(t('Internal server error.'));
+        break;
+    }
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     event.stopPropagation();
     if (!checked) return setErrorMessage(t('Terms of use error'));
-    if (formValues.password.length < 8)
-      return setErrorMessage(t('Password type error'));
-    if (formValues.password !== formValues.confirmPassword)
-      return setErrorMessage(t('Password confirmation error'));
+    if (formValues.password !== formValues.confirmPassword) {
+      setError('confirmPassword', t('Password confirmation error'));
+      return;
+    }
 
-    dispatch(
+    const res = await dispatch(
       userActions.register({
-        username: formValues.name,
+        username: formValues.username,
         email: formValues.email,
         password: formValues.password,
       }),
-    ).then((res) => {
-      if (userActions.register.rejected.match(res)) {
-        if (!res.payload) return setErrorMessage(t('Internal server error'));
-
-        return setErrorMessage(
-          errorHandler(
-            res.payload.response?.data.error,
-            res.payload.response?.data.message,
-          ),
-        );
-      }
-      if (userActions.register.fulfilled.match(res)) {
-        history.push('/login');
-      }
-    });
+    );
+    if (userActions.register.rejected.match(res)) {
+      const data = res.payload?.response?.data;
+      errorHandler(data?.error, data);
+    } else if (userActions.register.fulfilled.match(res)) {
+      history.push('/login');
+    }
   };
 
   const EmeraldCheckBox = withStyles({
@@ -100,77 +161,24 @@ export const RegisterPage = () => {
             <Trans i18nKey="Please fill out some info about you." />
           </div>
           <form onSubmit={handleSubmit}>
-            <label htmlFor="name">
-              <Trans i18nKey="Your name" />
-            </label>
-            <input
-              required
-              name="name"
-              id="name"
-              placeholder={t('Your name')}
-              value={formValues.name}
-              maxLength={255}
-              onChange={(e) =>
-                setFormValues({ ...formValues, name: e.currentTarget.value })
-              }
-            />
-
-            <label htmlFor="new-email">
-              <Trans i18nKey="Your email" />
-            </label>
-            <input
-              required
-              name="new-email"
-              id="new-email"
-              type="email"
-              placeholder={t('Example email')}
-              autoComplete="off"
-              value={formValues.email}
-              maxLength={255}
-              onChange={(e) =>
-                setFormValues({ ...formValues, email: e.currentTarget.value })
-              }
-            />
-
-            <label htmlFor="new-password">
-              <Trans i18nKey="Password" />
-            </label>
-            <input
-              required
-              name="new-password"
-              id="new-password"
-              placeholder="-"
-              type="password"
-              autoComplete="new-password"
-              value={formValues.password}
-              maxLength={72}
-              onChange={(e) =>
-                setFormValues({
-                  ...formValues,
-                  password: e.currentTarget.value,
-                })
-              }
-            />
-
-            <label htmlFor="confirm-password">
-              <Trans i18nKey="Confirm password" />
-            </label>
-            <input
-              required
-              name="confirm-password"
-              id="confirm-password"
-              placeholder="-"
-              type="password"
-              autoComplete="confirm-password"
-              value={formValues.confirmPassword}
-              maxLength={72}
-              onChange={(e) =>
-                setFormValues({
-                  ...formValues,
-                  confirmPassword: e.currentTarget.value,
-                })
-              }
-            />
+            {Object.entries(fields).map(([name, props]) => {
+              assert(knownField(name), `invalid field: ${name}`);
+              return (
+                <Input
+                  {...props}
+                  key={name}
+                  id={name}
+                  required
+                  value={formValues[name]}
+                  onChange={(e) =>
+                    setFormValues({
+                      ...formValues,
+                      [name]: e.currentTarget.value,
+                    })
+                  }
+                />
+              );
+            })}
 
             <div className={classes(css.termsLabel)}>
               <EmeraldCheckBox />
