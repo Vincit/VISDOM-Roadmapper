45e4bd6c67640d17cc8186edb1b931280fc0e2b7
PatricKangasmaki <80753884+PatricKangasmaki@users.noreply.github.com>
2021-07-13 11:39:30 +0300

    Update unratedTaskTable styles to match Figma design

--- frontend/src/components/TableUnratedTaskRow.tsx
+++ frontend/src/components/TableUnratedTaskRow.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useState, useEffect } from 'react';
-import { CheckCircle, Circle } from 'react-bootstrap-icons';
 import BuildIcon from '@material-ui/icons/Build';
 import PermIdentityIcon from '@material-ui/icons/PermIdentity';
 import Tooltip from '@material-ui/core/Tooltip';
@@ -11,28 +10,23 @@ import classNames from 'classnames';
 import { StylesProvider } from '@material-ui/core/styles';
 import { StoreDispatchType } from '../redux';
 import { modalsActions } from '../redux/modals';
-import { ModalTypes, modalLink } from '../redux/modals/types';
-import { roadmapsActions } from '../redux/roadmaps/index';
+import { ModalTypes } from '../redux/modals/types';
 import { Task, Customer, RoadmapUser } from '../redux/roadmaps/types';
 import { RootState } from '../redux/types';
 import { userInfoSelector } from '../redux/user/selectors';
 import { UserInfo } from '../redux/user/types';
-import { RoleType } from '../../../shared/types/customTypes';
+import {
+  RoleType,
+  TaskRatingDimension,
+} from '../../../shared/types/customTypes';
 import {
   roadmapUsersSelector,
   allCustomersSelector,
 } from '../redux/roadmaps/selectors';
-import { isUnrated } from '../utils/TaskUtils';
-import {
-  DeleteButton,
-  EditButton,
-  InfoButton,
-  RatingsButton,
-} from './forms/SvgButton';
-import { TaskRatingsText } from './TaskRatingsText';
 import { Dot } from './Dot';
 import { getType } from '../utils/UserUtils';
 import css from './TableUnratedTaskRow.module.scss';
+import { taskAwaitsRatings, totalRatingsByDimension } from '../utils/TaskUtils';
 
 const classes = classNames.bind(css);
 
@@ -42,7 +36,7 @@ interface TableTaskRowProps {
 
 export const TableUnratedTaskRow: React.FC<TableTaskRowProps> = ({ task }) => {
   const dispatch = useDispatch<StoreDispatchType>();
-  const { id, name, completed, roadmapId, description, createdAt } = task;
+  const { name, roadmapId } = task;
   const userInfo = useSelector<RootState, UserInfo | undefined>(
     userInfoSelector,
     shallowEqual,
@@ -63,6 +57,13 @@ export const TableUnratedTaskRow: React.FC<TableTaskRowProps> = ({ task }) => {
     RoadmapUser[] | undefined
   >([]);
   const [userRatingMissing, setUserRatingMissing] = useState<boolean>(true);
+
+  const totalRatings = totalRatingsByDimension(task);
+  const noRatings = { sum: 0, count: 1 };
+  const valueRatings =
+    totalRatings.get(TaskRatingDimension.BusinessValue) ?? noRatings;
+  const workRatings =
+    totalRatings.get(TaskRatingDimension.RequiredWork) ?? noRatings;
 
   /*
     AdminUsers can see missing customer and developer ratings
@@ -127,12 +128,6 @@ export const TableUnratedTaskRow: React.FC<TableTaskRowProps> = ({ task }) => {
     }
   }, [task.ratings, allCustomers, allUsers, userInfo, type, task.roadmapId]);
 
-  const deleteTaskClicked = (e: React.MouseEvent<unknown, MouseEvent>) => {
-    e.preventDefault();
-    e.stopPropagation();
-    dispatch(roadmapsActions.deleteTask({ id, roadmapId }));
-  };
-
   const openModal = (modalType: ModalTypes) => (e: React.SyntheticEvent) => {
     e.preventDefault();
     e.stopPropagation();
@@ -146,41 +141,27 @@ export const TableUnratedTaskRow: React.FC<TableTaskRowProps> = ({ task }) => {
     );
   };
 
-  const editTaskClicked = openModal(ModalTypes.EDIT_TASK_MODAL);
-  const rateTaskClicked = openModal(ModalTypes.RATE_TASK_MODAL);
-  const taskRatingDetailsClicked = openModal(
-    ModalTypes.TASK_RATINGS_INFO_MODAL,
-  );
-  const taskDetailsClicked = openModal(ModalTypes.TASK_INFO_MODAL);
-
-  const toggleCompletedClicked = (e: React.MouseEvent<unknown, MouseEvent>) => {
-    e.preventDefault();
-    e.stopPropagation();
-    dispatch(roadmapsActions.patchTask({ id, completed: !completed }));
+  const numFormat = (num: number) => {
+    if (Number.isNaN(num)) return 0;
+    return num.toLocaleString(undefined, {
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 1,
+    });
   };
 
   return (
     <tr
       className={classes(css.styledTr, css.clickable)}
-      onClick={taskDetailsClicked}
+      onClick={openModal(ModalTypes.TASK_INFO_MODAL)}
     >
-      <td
-        className="styledTd clickable textAlignCenter"
-        onClick={toggleCompletedClicked}
-      >
-        {completed ? (
-          <CheckCircle onClick={toggleCompletedClicked} />
-        ) : (
-          <Circle onClick={toggleCompletedClicked} />
-        )}
+      <td className={classes(css.taskTitle)}>{name}</td>
+      <td className={classes(css.unratedTd)}>
+        {numFormat(valueRatings.sum / valueRatings.count)}
       </td>
-      <td className="styledTd">{name}</td>
-      <td className="styledTd">
-        {description.length > 75
-          ? `${description.slice(0, 75)}...`
-          : description}
+      <td className={classes(css.unratedTd)}>
+        {numFormat(workRatings.sum / workRatings.count)}
       </td>
-      <td className="styledTd">
+      <td className={classes(css.unratedTd)}>
         <div className={classes(css.missingContainer)}>
           <StylesProvider injectFirst>
             {missingRatings && (
@@ -241,50 +222,34 @@ export const TableUnratedTaskRow: React.FC<TableTaskRowProps> = ({ task }) => {
           </StylesProvider>
         </div>
       </td>
-      <td className="styledTd nowrap">
-        <TaskRatingsText task={task} />
-      </td>
-      <td className="styledTd">{new Date(createdAt).toLocaleDateString()}</td>
-      <td className="styledTd textAlignEnd nowrap" style={{ width: '202px' }}>
-        {userInfo && isUnrated(userInfo)(task) && (
-          <a
-            href={modalLink(ModalTypes.TASK_RATINGS_INFO_MODAL, {
-              taskId: task.id,
-            })}
+      <td className={classes(css.buttonContainer)}>
+        {type === RoleType.Admin && (
+          <button
+            style={{ marginRight: '10px' }}
+            className={classes(css['button-small-outlined'])}
+            type="button"
+            /* onClick={openModal(ModalTypes.NOTIFY_USERS_MODAL)} */
           >
-            <button
-              className={classes(css['button-small-filled'])}
-              type="button"
-              onClick={rateTaskClicked}
-            >
-              <Trans i18nKey="Rate" />
-            </button>
-          </a>
+            <Trans i18nKey="Notify" />
+          </button>
         )}
-        <div className={classes(css.buttonWrapper)}>
-          <RatingsButton
-            onClick={taskRatingDetailsClicked}
-            href={modalLink(ModalTypes.TASK_RATINGS_INFO_MODAL, {
-              taskId: task.id,
-            })}
-          />
-          <InfoButton
-            onClick={taskDetailsClicked}
-            href={modalLink(ModalTypes.TASK_INFO_MODAL, { taskId: task.id })}
-          />
-          {type === RoleType.Admin && (
-            <>
-              <EditButton
-                fontSize="default"
-                onClick={editTaskClicked}
-                href={modalLink(ModalTypes.EDIT_TASK_MODAL, {
-                  taskId: task.id,
-                })}
-              />
-              <DeleteButton type="outlined" onClick={deleteTaskClicked} />
-            </>
-          )}
-        </div>
+        {taskAwaitsRatings(task, userInfo) ? (
+          <button
+            className={classes(css['button-small-filled'])}
+            type="button"
+            onClick={openModal(ModalTypes.RATE_TASK_MODAL)}
+          >
+            <Trans i18nKey="Rate" />
+          </button>
+        ) : (
+          <button
+            className={classes(css['button-small-filled'])}
+            type="button"
+            disabled
+          >
+            <Trans i18nKey="Rate" />
+          </button>
+        )}
       </td>
     </tr>
   );
