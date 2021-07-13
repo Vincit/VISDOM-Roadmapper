ef28e62ef659e6d840d4c3ee14e09bd30044def4
Iida Kainu <iida.kainu@vincit.fi>
2021-07-13 15:44:24 +0300

    Fix roadmap deletion
    Add delete rule 'cascade' to versions' roadmapId column

---
+++ server/src/migrations/20210713135033_addRoadmapIdCascadeOnDelete.ts
@@ -0,0 +1,28 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema.alterTable('versions', (table) => {
+    table.dropForeign(['roadmapId']);
+    table.dropIndex('roadmapId');
+    table
+      .integer('roadmapId')
+      .references('id')
+      .inTable('roadmaps')
+      .onDelete('CASCADE')
+      .index()
+      .alter();
+  });
+}
+
+export async function down(knex: Knex): Promise<any> {
+  return knex.schema.alterTable('versions', (table) => {
+    table.dropForeign(['roadmapId']);
+    table.dropIndex('roadmapId');
+    table
+      .integer('roadmapId')
+      .references('id')
+      .inTable('roadmaps')
+      .index()
+      .alter();
+  });
+}
