96a34873d2f0eab1100d7241d7332e00d2297967
Juho Eerola <juho.eerola@vincit.fi>
2021-07-21 20:23:12 +0300

    Update server eslint configuration

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,58 +1,42 @@
 module.exports = {
   env: {
-    browser: true,
+    node: true,
     es6: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'plugin:jest/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier', 'jest'],
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/no-unused-vars': 'error',
-    'react/jsx-filename-extension': ['error', { extensions: ['.tsx', '.ts'] }],
-    'import/extensions': [
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
       'error',
-      'ignorePackages',
       {
-        ts: 'never',
-        tsx: 'never',
+        args: 'after-used',
+        vars: 'all',
+        ignoreRestSiblings: true,
       },
     ],
-    'no-param-reassign': [
-      'error',
-      // ignore 'state' as `redux-toolkit` handles state modifications with `immer`
-      { props: true, ignorePropertyModificationsFor: ['state'] },
-    ],
-    'import/prefer-default-export': 'off',
-    'import/no-default-export': 'error',
-    'react/jsx-one-expression-per-line': 'off', // conflicts with prettier
-    'react/prop-types': 'off', // not needed with TypeScript
-    'react/jsx-curly-newline': 'off', // conflicts with prettier
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'error',
   },
-  settings: {
-    'import/resolver': {
-      typescript: {},
+  overrides: [
+    {
+      files: ['**/migrations/*'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          {
+            argsIgnorePattern: '^knex$',
+          },
+        ],
+      },
     },
-  },
+  ],
 };
