7529a2bd669ebcb14597b596f99ff5b3ec76730d
Patric Kangasmäki <patric.kangasmaki@vincit.fi>
2021-09-22 08:00:28 +0300

    Replace PlannerPageNavbar with PageNavbar to allow dynamical use
    Previously you could only use the navbar in planner page. Now it takes headers and url as params and you can use it anywhere on the app.

---
+++ frontend/src/components/PageNavBar.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react';
+import { Link, useLocation, useRouteMatch } from 'react-router-dom';
+import classNames from 'classnames';
+import css from './PageNavBar.module.scss';
+
+const classes = classNames.bind(css);
+
+interface Header {
+  url: string;
+  text: string;
+}
+
+export const PageNavBar: FC<{
+  headers: Header[];
+}> = ({ headers }) => {
+  const { url } = useRouteMatch();
+  const { pathname } = useLocation();
+  return (
+    <div className={classes(css.navbar)}>
+      {headers.map((header: any) => (
+        <Link
+          className={classes(css.navbarButton, {
+            [css.highlight]: !!pathname.startsWith(url + header.url),
+          })}
+          to={url + header.url}
+        >
+          {header.text}
+        </Link>
+      ))}
+      <div className={classes(css.navbarFiller)} />
+    </div>
+  );
+};
