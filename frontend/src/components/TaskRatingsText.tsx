f85a76834ecbed26cb8e2490170c2fd3a561e73a
Patric Kangasmäki <patric.kangasmaki@vincit.fi>
2021-09-22 08:38:17 +0300

    Add conditional fill for icons
    Add possibility to pass a parameter "selected" which will cause the icons to be color white

--- frontend/src/components/TaskRatingsText.tsx
+++ frontend/src/components/TaskRatingsText.tsx
@@ -9,7 +9,10 @@ import css from './TaskRatingsText.module.scss';
 
 const classes = classNames.bind(css);
 
-export const TaskRatingsText: FC<{ task: Task }> = ({ task }) => {
+export const TaskRatingsText: FC<{ task: Task; selected?: boolean }> = ({
+  task,
+  selected,
+}) => {
   const { work, value } = averageValueAndWork([task]);
   const numFormat = new Intl.NumberFormat(undefined, {
     minimumFractionDigits: 0,
@@ -18,11 +21,11 @@ export const TaskRatingsText: FC<{ task: Task }> = ({ task }) => {
   return (
     <div className={classes(css.taskRatingRow)}>
       <div className={classes(css.taskRating)}>
-        <BusinessIcon size="xxsmall" color={colors.azure} />
+        <BusinessIcon color={selected ? '#FFFFFF' : colors.azure} />
         {numFormat.format(value)}
       </div>
       <div className={classes(css.taskRating)}>
-        <WorkRoundIcon size="xxsmall" color={colors.azure} />
+        <WorkRoundIcon color={selected ? '#FFFFFF' : colors.azure} />
         {numFormat.format(work)}
       </div>
     </div>
