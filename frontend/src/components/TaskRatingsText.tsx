27ea89809be6f214bc3eac7ff082502c613745ff
Iida Kainu <iida.kainu@vincit.fi>
2021-10-07 14:21:27 +0300

    Add code review changes

--- frontend/src/components/TaskRatingsText.tsx
+++ frontend/src/components/TaskRatingsText.tsx
@@ -9,23 +9,30 @@ import css from './TaskRatingsText.module.scss';
 
 const classes = classNames.bind(css);
 
-export const TaskRatingsText: FC<{ task: Task; selected?: boolean }> = ({
-  task,
-  selected,
-}) => {
+export const TaskRatingsText: FC<{
+  task: Task;
+  selected?: boolean;
+  largeIcons?: true;
+}> = ({ task, selected, largeIcons }) => {
   const { work, value } = averageValueAndWork([task]);
   const numFormat = new Intl.NumberFormat(undefined, {
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 2,
+    minimumFractionDigits: 1,
+    maximumFractionDigits: 1,
   });
   return (
     <div className={classes(css.taskRatingRow)}>
       <div className={classes(css.taskRating)}>
-        <BusinessIcon color={selected ? '#FFFFFF' : colors.azure} />
+        <BusinessIcon
+          size={largeIcons ? 'small' : 'xxsmall'}
+          color={selected ? colors.white : colors.azure}
+        />
         {numFormat.format(value)}
       </div>
       <div className={classes(css.taskRating)}>
-        <WorkRoundIcon color={selected ? '#FFFFFF' : colors.azure} />
+        <WorkRoundIcon
+          size={largeIcons ? 'small' : 'xxsmall'}
+          color={selected ? colors.white : colors.azure}
+        />
         {numFormat.format(work)}
       </div>
     </div>
