a669c80117c39c2ee5220150dd9ecbb1f9c41499
Patric Kangasmäki <patric.kangasmaki@vincit.fi>
2021-10-26 15:50:12 +0300

    Add functionality for removing task relations through task-map

--- frontend/src/api/api.tsx
+++ frontend/src/api/api.tsx
@@ -25,6 +25,7 @@ import {
   VersionRequest,
   Invitation,
   InvitationRequest,
+  TaskRelation,
 } from '../redux/roadmaps/types';
 import { IntegrationBoard, Integrations } from '../redux/types';
 import {
@@ -389,6 +390,17 @@ const addTaskRelation = async (relation: TaskRelationRequest) => {
   return response.status === 200;
 };
 
+const removeTaskRelation = async (
+  roadmapId: number,
+  relation: TaskRelation,
+) => {
+  const response = await axios.delete(
+    `/roadmaps/${roadmapId}/tasks/relations`,
+    { data: relation },
+  );
+  return response.status === 200;
+};
+
 export const api = {
   getRoadmaps,
   addRoadmap,
@@ -435,4 +447,5 @@ export const api = {
   verifyEmail,
   sendEmailVerificationLink,
   addTaskRelation,
+  removeTaskRelation,
 };
