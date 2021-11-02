8265b280b8973ef54bc3d8ce3fe152a1cee67acd
Iida Kainu <iida.kainu@vincit.fi>
2021-11-02 11:14:39 +0200

    Move checkmark to left of task name
    Green checkmark is moved to the left of task name in task-map.

--- frontend/src/components/TaskRatingsText.tsx
+++ frontend/src/components/TaskRatingsText.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react';
 import classNames from 'classnames';
-import DoneAllIcon from '@material-ui/icons/DoneAll';
 import { BusinessIcon, WorkRoundIcon } from './RoleIcons';
 import { Task } from '../redux/roadmaps/types';
 import { averageValueAndWork } from '../utils/TaskUtils';
@@ -13,9 +12,8 @@ const classes = classNames.bind(css);
 export const TaskRatingsText: FC<{
   task: Task;
   selected?: boolean;
-  completed?: boolean;
   largeIcons?: true;
-}> = ({ task, selected, completed, largeIcons }) => {
+}> = ({ task, selected, largeIcons }) => {
   const { work, value } = averageValueAndWork([task]);
   const numFormat = new Intl.NumberFormat(undefined, {
     minimumFractionDigits: 1,
@@ -24,11 +22,6 @@ export const TaskRatingsText: FC<{
   const color = selected ? colors.white : colors.azure;
   return (
     <div className={classes(css.taskRatingRow)}>
-      {completed && (
-        <DoneAllIcon
-          className={classes(css.doneIcon, { [css.white]: selected })}
-        />
-      )}
       <div className={classes(css.taskRating)}>
         <BusinessIcon size={largeIcons ? 'small' : 'xxsmall'} color={color} />
         {numFormat.format(value)}
