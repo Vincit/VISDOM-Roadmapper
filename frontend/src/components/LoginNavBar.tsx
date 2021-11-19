eaf42d9090b8f3a3aa9e0c12ef793c2db9a06fad
Juho Eerola <juho.eerola@vincit.fi>
2021-11-19 14:37:43 +0200

    Rework login navbar

--- frontend/src/components/LoginNavBar.tsx
+++ frontend/src/components/LoginNavBar.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import classNames from 'classnames';
 import { Trans } from 'react-i18next';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, matchPath } from 'react-router-dom';
 import { paths } from '../routers/paths';
 import { ReactComponent as VisdomLogo } from '../icons/visdom_icon.svg';
 
@@ -9,45 +9,88 @@ import css from './LoginNavBar.module.scss';
 
 const classes = classNames.bind(css);
 
-export const LoginNavBar: FC<{
-  type: 'landing' | 'login' | 'register' | 'getStarted';
-}> = ({ type }) => {
-  const { search } = useLocation();
-  return (
-    <div
-      className={classes(css.loginNavBar, {
-        [css.landingNavBar]: type === 'landing',
-      })}
-    >
-      {type === 'landing' || type === 'getStarted' ? (
+const navBars: { path: string; component: FC<{ search: string }> }[] = [
+  {
+    path: paths.getStarted,
+    component: () => (
+      <div className={classes(css.loginNavBar)}>
         <VisdomLogo />
-      ) : (
-        <Link to={paths.home} className={classes(css.visdomLogo)}>
-          <VisdomLogo />
-        </Link>
-      )}
-      <span />
-      {type === 'getStarted' && (
+        <span />
         <Link className={classes(css.loginButton)} to={paths.logoutPage}>
           <Trans i18nKey="Logout" />
         </Link>
-      )}
-      {type === 'login' && (
+      </div>
+    ),
+  },
+  {
+    path: paths.loginPage,
+    component: ({ search }) => (
+      <div className={classes(css.loginNavBar)}>
+        <Link to={paths.home} className={classes(css.visdomLogo)}>
+          <VisdomLogo />
+        </Link>
+        <span />
         <Link
           className={classes(css.loginButton)}
           to={paths.registerPage + search}
         >
           <Trans i18nKey="Register" />
         </Link>
-      )}
-      {(type === 'register' || type === 'landing') && (
+      </div>
+    ),
+  },
+  {
+    path: paths.registerPage,
+    component: ({ search }) => (
+      <div className={classes(css.loginNavBar)}>
+        <Link to={paths.home} className={classes(css.visdomLogo)}>
+          <VisdomLogo />
+        </Link>
+        <span />
         <Link
           className={classes(css.loginButton)}
           to={paths.loginPage + search}
         >
           <Trans i18nKey="Login" />
         </Link>
-      )}
+      </div>
+    ),
+  },
+  {
+    path: paths.emailVerification,
+    component: () => (
+      <div className={classes(css.loginNavBar)}>
+        <Link to={paths.home} className={classes(css.visdomLogo)}>
+          <VisdomLogo />
+        </Link>
+        <span />
+        <Link className={classes(css.loginButton)} to={paths.logoutPage}>
+          <Trans i18nKey="Logout" />
+        </Link>
+      </div>
+    ),
+  },
+];
+
+export const findLoginNavBar = (pathname: string) =>
+  navBars.find(({ path }) =>
+    matchPath(pathname, { path, strict: true, exact: true }),
+  )?.component;
+
+export const LoginNavBar = () => {
+  const { search, pathname } = useLocation();
+  return findLoginNavBar(pathname)?.({ search }) ?? null;
+};
+
+export const LandingPageNavBar = () => {
+  const { search } = useLocation();
+  return (
+    <div className={classes(css.loginNavBar, css.landingNavBar)}>
+      <VisdomLogo />
+      <span />
+      <Link className={classes(css.loginButton)} to={paths.loginPage + search}>
+        <Trans i18nKey="Login" />
+      </Link>
     </div>
   );
 };
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,38 +1,22 @@
 import { Trans } from 'react-i18next';
-import { shallowEqual, useSelector } from 'react-redux';
 import { Link, useLocation } from 'react-router-dom';
 import PermIdentityIcon from '@material-ui/icons/PermIdentity';
 import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
 import classNames from 'classnames';
 import { ReactComponent as CornerPiece } from '../icons/corner_rounder.svg';
 import { ReactComponent as VisdomLogo } from '../icons/visdom_icon.svg';
-import { RootState } from '../redux/types';
-import { userInfoSelector } from '../redux/user/selectors';
-import { UserInfo } from '../redux/user/types';
 import { paths } from '../routers/paths';
 import { RoadmapSelectorWidget } from './RoadmapSelectorWidget';
 import css from './NavBar.module.scss';
-import { LoginNavBar } from './LoginNavBar';
+import { findLoginNavBar } from './LoginNavBar';
 
 const classes = classNames.bind(css);
 
 export const NavBar = () => {
-  const userInfo = useSelector<RootState, UserInfo | undefined>(
-    userInfoSelector,
-    shallowEqual,
-  );
-  const { pathname } = useLocation();
+  const { search, pathname } = useLocation();
 
-  if (!userInfo) {
-    return pathname.startsWith(paths.loginPage) ? (
-      <LoginNavBar type="login" />
-    ) : (
-      <LoginNavBar type="register" />
-    );
-  }
-
-  if (pathname.startsWith(paths.getStarted))
-    return <LoginNavBar type="getStarted" />;
+  const loginNavBar = findLoginNavBar(pathname);
+  if (loginNavBar) return loginNavBar({ search });
 
   return (
     <div className={classes(css.navBarDiv)}>
