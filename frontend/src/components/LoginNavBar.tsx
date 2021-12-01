e2b35c0af30d345dc18d2751a68a55ac71cc8797
Juho Eerola <juho.eerola@vincit.fi>
2021-12-01 11:19:16 +0200

    Reduce duplication in navBar components

--- frontend/src/components/LoginNavBar.tsx
+++ frontend/src/components/LoginNavBar.tsx
@@ -1,4 +1,3 @@
-import { FC } from 'react';
 import classNames from 'classnames';
 import { Trans } from 'react-i18next';
 import { Link, useLocation, matchPath } from 'react-router-dom';
@@ -9,87 +8,58 @@ import css from './LoginNavBar.module.scss';
 
 const classes = classNames.bind(css);
 
-const navBars: { path: string; component: FC<{ search: string }> }[] = [
+const navBars: {
+  path: string;
+  button: { to: (search: string) => string; label: string };
+  linkHome?: boolean;
+}[] = [
   {
     path: paths.getStarted,
-    component: () => (
-      <div className={classes(css.loginNavBar)}>
-        <VisdomLogo />
-        <span />
-        <Link className={classes(css.loginButton)} to={paths.logoutPage}>
-          <Trans i18nKey="Logout" />
-        </Link>
-      </div>
-    ),
+    button: { to: () => paths.logoutPage, label: 'Logout' },
   },
   {
     path: paths.loginPage,
-    component: ({ search }) => (
-      <div className={classes(css.loginNavBar)}>
-        <Link to={paths.home} className={classes(css.visdomLogo)}>
-          <VisdomLogo />
-        </Link>
-        <span />
-        <Link
-          className={classes(css.loginButton)}
-          to={paths.registerPage + search}
-        >
-          <Trans i18nKey="Register" />
-        </Link>
-      </div>
-    ),
+    button: { to: (search) => paths.registerPage + search, label: 'Register' },
+    linkHome: true,
   },
   {
     path: paths.registerPage,
-    component: ({ search }) => (
-      <div className={classes(css.loginNavBar)}>
-        <Link to={paths.home} className={classes(css.visdomLogo)}>
-          <VisdomLogo />
-        </Link>
-        <span />
-        <Link
-          className={classes(css.loginButton)}
-          to={paths.loginPage + search}
-        >
-          <Trans i18nKey="Login" />
-        </Link>
-      </div>
-    ),
+    button: { to: (search) => paths.loginPage + search, label: 'Login' },
+    linkHome: true,
   },
   {
     path: paths.emailVerification,
-    component: () => (
-      <div className={classes(css.loginNavBar)}>
-        <Link to={paths.home} className={classes(css.visdomLogo)}>
-          <VisdomLogo />
-        </Link>
-        <span />
-        <Link className={classes(css.loginButton)} to={paths.logoutPage}>
-          <Trans i18nKey="Logout" />
-        </Link>
-      </div>
-    ),
+    button: { to: () => paths.logoutPage, label: 'Logout' },
+    linkHome: true,
   },
   {
     path: paths.verifyEmail,
-    component: () => (
-      <div className={classes(css.loginNavBar)}>
-        <Link to={paths.home} className={classes(css.visdomLogo)}>
-          <VisdomLogo />
-        </Link>
-        <span />
-        <Link className={classes(css.loginButton)} to={paths.logoutPage}>
-          <Trans i18nKey="Logout" />
-        </Link>
-      </div>
-    ),
+    button: { to: () => paths.logoutPage, label: 'Logout' },
+    linkHome: true,
   },
 ];
 
-export const findLoginNavBar = (pathname: string) =>
-  navBars.find(({ path }) =>
+export const findLoginNavBar = (pathname: string) => {
+  const bar = navBars.find(({ path }) =>
     matchPath(pathname, { path, strict: true, exact: true }),
-  )?.component;
+  );
+  if (!bar) return undefined;
+  return ({ search }: { search: string }) => (
+    <div className={classes(css.loginNavBar)}>
+      {bar.linkHome ? (
+        <Link to={paths.home} className={classes(css.visdomLogo)}>
+          <VisdomLogo />
+        </Link>
+      ) : (
+        <VisdomLogo />
+      )}
+      <span />
+      <Link className={classes(css.loginButton)} to={bar.button.to(search)}>
+        <Trans i18nKey={bar.button.label} />
+      </Link>
+    </div>
+  );
+};
 
 export const LoginNavBar = () => {
   const { search, pathname } = useLocation();
