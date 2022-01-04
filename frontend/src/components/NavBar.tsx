b2331807fcb0fc5295e4081eb55fc1eda79a4948
Aleksi Kytölä <aleksi.kytola@vincit.fi>
2022-01-04 10:25:33 +0200

    Display current user info & role in navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -3,26 +3,26 @@ import { shallowEqual, useSelector } from 'react-redux';
 import { Link, useLocation } from 'react-router-dom';
 import PermIdentityIcon from '@material-ui/icons/PermIdentity';
 import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import StarSharpIcon from '@material-ui/icons/StarSharp';
+import BuildSharpIcon from '@material-ui/icons/BuildSharp';
 import classNames from 'classnames';
-import { RootState } from '../redux/types';
-import { UserInfo } from '../redux/user/types';
-import { userInfoSelector } from '../redux/user/selectors';
 import { ReactComponent as CornerPiece } from '../icons/corner_rounder.svg';
 import { ReactComponent as VisdomLogo } from '../icons/visdom_icon.svg';
 import { paths } from '../routers/paths';
 import { RoadmapSelectorWidget } from './RoadmapSelectorWidget';
 import css from './NavBar.module.scss';
 import { findLoginNavBar } from './LoginNavBar';
+import { userInfoSelector, userRoleSelector } from '../redux/user/selectors';
+import { BusinessIcon } from './RoleIcons';
+import { RoleType } from '../../../shared/types/customTypes';
 
 const classes = classNames.bind(css);
 
 export const NavBar = () => {
   const { search, pathname } = useLocation();
-  const loggedIn = !!useSelector<RootState, UserInfo | undefined>(
-    userInfoSelector,
-    shallowEqual,
-  );
-
+  const userInfo = useSelector(userInfoSelector, shallowEqual);
+  const userRole = useSelector(userRoleSelector, shallowEqual);
+  const loggedIn = !!userInfo;
   const loginNavBar = findLoginNavBar(pathname, loggedIn);
   if (loginNavBar) return loginNavBar({ search });
 
@@ -41,6 +41,17 @@ export const NavBar = () => {
         </div>
         <RoadmapSelectorWidget />
         <div className={classes(css.navBarDivider)} />
+        {userInfo && (
+          <div className={classes(css.userInfoContainer)}>
+            <div className={classes(css.memberIcon)}>
+              {userRole === RoleType.Admin && <StarSharpIcon />}
+              {userRole === RoleType.Developer && <BuildSharpIcon />}
+              {userRole === RoleType.Business && <BusinessIcon />}
+            </div>
+            {userInfo.email}
+          </div>
+        )}
+
         <Link className={classes(css.navBarLink)} to={paths.userInfo}>
           <PermIdentityIcon className={classes(css.icon)} />
         </Link>
