6a49d9af8e44e799cacd9ad6932cc53286f0b786
Iida Kainu <iida.kainu@vincit.fi>
2022-01-25 15:38:53 +0200

    Use MissingRatings & TaskModalButtons in task rows

--- frontend/src/components/TaskTableUnrated.tsx
+++ frontend/src/components/TaskTableUnrated.tsx
@@ -1,8 +1,6 @@
-import { SyntheticEvent, useState, useEffect } from 'react';
+import { SyntheticEvent } from 'react';
 import { Link } from 'react-router-dom';
-import BuildIcon from '@material-ui/icons/Build';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
-import Tooltip from '@material-ui/core/Tooltip';
 import { Trans } from 'react-i18next';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames';
@@ -12,29 +10,22 @@ import { paths } from '../routers/paths';
 import { StoreDispatchType } from '../redux';
 import { modalsActions } from '../redux/modals';
 import { ModalTypes, ShowModalPayload } from './modals/types';
-import { Customer, RoadmapUser, Task } from '../redux/roadmaps/types';
+import { Task } from '../redux/roadmaps/types';
 import { RootState } from '../redux/types';
 import { userInfoSelector } from '../redux/user/selectors';
 import { UserInfo } from '../redux/user/types';
 import { RoleType } from '../../../shared/types/customTypes';
-import {
-  roadmapUsersSelector,
-  allCustomersSelector,
-} from '../redux/roadmaps/selectors';
-import { Dot } from './Dot';
-import { getType, representsCustomers } from '../utils/UserUtils';
+import { getType } from '../utils/UserUtils';
 import css from './TaskTable.module.scss';
 import {
-  awaitsUserRatings,
   averageValueAndWork,
-  missingCustomer,
-  missingDeveloper,
   SortingTypes,
-  ratedByCustomer,
   taskSort,
 } from '../utils/TaskUtils';
 import { table, TableRow } from './Table';
 import { DeleteButton } from './forms/SvgButton';
+import { MissingRatings } from './MissingRatings';
+import { TaskModalButtons } from './TaskModalButtons';
 
 const classes = classNames.bind(css);
 
@@ -51,63 +42,14 @@ const TableUnratedTaskRow: TableRow<Task> = ({ item: task, style }) => {
     shallowEqual,
   );
   const type = getType(userInfo, roadmapId);
-  const allUsers = useSelector<RootState, RoadmapUser[] | undefined>(
-    roadmapUsersSelector,
-    shallowEqual,
-  );
-  const allCustomers = useSelector<RootState, Customer[] | undefined>(
-    allCustomersSelector,
-    shallowEqual,
-  );
-  const [missingRatings, setMissingRatings] = useState<Customer[] | undefined>(
-    undefined,
-  );
-  const [missingDevRatings, setMissingDevRatings] = useState<
-    RoadmapUser[] | undefined
-  >([]);
 
   const { value, work } = averageValueAndWork([task]);
-
-  /*
-    AdminUsers can see missing customer and developer ratings
-    BusinessUser can see their missing customer ratings
-    DeveloperUser can see missing developer ratings
-  */
-  useEffect(() => {
-    if (type === RoleType.Admin && allCustomers) {
-      setMissingRatings(allCustomers.filter(missingCustomer(task)));
-    }
-
-    if ((type === RoleType.Admin || type === RoleType.Developer) && allUsers) {
-      setMissingDevRatings(allUsers.filter(missingDeveloper(task)));
-    }
-
-    if (type === RoleType.Business) {
-      setMissingRatings(
-        userInfo?.representativeFor?.filter(
-          (customer) =>
-            roadmapId === customer.roadmapId &&
-            !ratedByCustomer(customer, userInfo)(task),
-        ),
-      );
-    }
-  }, [task, allCustomers, allUsers, userInfo, type, roadmapId]);
 
   const openModal = (payload: ShowModalPayload) => (e: SyntheticEvent) => {
     e.preventDefault();
     e.stopPropagation();
     dispatch(modalsActions.showModal(payload));
   };
-
-  const openNotifyModal = openModal({
-    modalType: ModalTypes.NOTIFY_USERS_MODAL,
-    modalProps: { taskId: task.id },
-  });
-
-  const openRateModal = openModal({
-    modalType: ModalTypes.RATE_TASK_MODAL,
-    modalProps: { taskId: task.id, edit: false },
-  });
 
   const handleTaskDelete = openModal({
     modalType: ModalTypes.REMOVE_TASK_MODAL,
@@ -123,67 +65,11 @@ const TableUnratedTaskRow: TableRow<Task> = ({ item: task, style }) => {
         <div className={classes(css.taskTitle)}>{name}</div>
         <div>{numFormat.format(value)}</div>
         <div>{numFormat.format(work)}</div>
-        <div>
-          <div className={classes(css.missingContainer)}>
-            {missingRatings?.map((customer) => (
-              <Tooltip
-                classes={{
-                  arrow: classes(css.tooltipArrow),
-                  tooltip: classes(css.tooltip),
-                }}
-                key={customer.id}
-                title={customer.name}
-                placement="top"
-                arrow
-              >
-                <div className={classes(css.dotContainer)}>
-                  <Dot fill={customer.color} />
-                </div>
-              </Tooltip>
-            ))}
-            {missingDevRatings && (
-              <div>
-                {missingDevRatings.map(({ email }) => (
-                  <Tooltip
-                    classes={{
-                      arrow: classes(css.tooltipArrow),
-                      tooltip: classes(css.tooltip),
-                    }}
-                    key={email}
-                    title={email}
-                    placement="top"
-                    arrow
-                  >
-                    <BuildIcon className={classes(css.developerIcon)} />
-                  </Tooltip>
-                ))}
-              </div>
-            )}
-          </div>
+        <div className={classes(css.missingRatings)}>
+          <MissingRatings task={task} />
         </div>
         <div className={classes(css.buttonContainer)}>
-          {type === RoleType.Admin && (
-            <button
-              style={{ marginRight: '10px' }}
-              className={classes(css['button-small-outlined'])}
-              type="button"
-              onClick={openNotifyModal}
-            >
-              <Trans i18nKey="Notify" />
-            </button>
-          )}
-          {userInfo &&
-            (type === RoleType.Developer ||
-              representsCustomers(userInfo, roadmapId)) && (
-              <button
-                className={classes(css['button-small-filled'])}
-                type="button"
-                disabled={!awaitsUserRatings(userInfo, roadmapId)(task)}
-                onClick={openRateModal}
-              >
-                <Trans i18nKey="Rate" />
-              </button>
-            )}
+          <TaskModalButtons task={task} />
           {type === RoleType.Admin && (
             <div className={classes(css.deleteIcon)}>
               <DeleteButton onClick={handleTaskDelete} />
