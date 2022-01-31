ecd51d3f60d5aadcad3035fdd9f37133816deb2b
Aleksi Kytölä <aleksi.kytola@vincit.fi>
2022-01-31 17:18:42 +0200

    Add util to track sockets of users

--- server/src/utils/socketIoUtils.ts
+++ server/src/utils/socketIoUtils.ts
@@ -1,4 +1,6 @@
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+
+const userToSocketMap: Record<number, Socket[]> = {};
 
 export const registerSocketCallbacks = (io: Server) => {
   io.on('connection', (socket) => {
@@ -11,5 +13,35 @@ export const registerSocketCallbacks = (io: Server) => {
       );
       return;
     }
+
+    // Track all sockets this userid has opened
+    if (!userToSocketMap[socket.data.user])
+      userToSocketMap[socket.data.user] = [];
+    userToSocketMap[socket.data.user].push(socket);
+
+    socket.on('disconnect', () => {
+      if (!socket.data.user) {
+        console.error(
+          'Socket disconnected without associated user. This should never happen',
+        );
+        return;
+      }
+
+      // Remove from list of sockets this userid has opened
+      const sockets = userToSocketMap[socket.data.user];
+      if (sockets) {
+        const filtered = sockets.filter((s) => s != socket);
+
+        if (filtered.length === 0) {
+          delete userToSocketMap[socket.data.user];
+        } else {
+          userToSocketMap[socket.data.user] = filtered;
+        }
+      }
+    });
   });
+};
+
+export const getSocketsByUser = (userId: number) => {
+  return userToSocketMap[userId];
 };
