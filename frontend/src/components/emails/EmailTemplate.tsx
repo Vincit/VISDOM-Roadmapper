1baf8fe90ec6700cd53ec08e2d729ed5669465cd
Patric Kangasmäki <patric.kangasmaki@vincit.fi>
2022-01-31 11:20:02 +0200

    Create template for email messages

---
+++ frontend/src/components/emails/EmailTemplate.tsx
@@ -0,0 +1,66 @@
+import { FC } from 'react';
+import { colorForest } from './emailStyles';
+import { baseUrl } from './emailTestEnv';
+
+const container = {
+  backgroundColor: '#f5f5f5',
+  textAlign: 'center' as 'center',
+};
+
+const visdomLogo = {
+  height: '32px',
+  width: 'auto',
+};
+
+const formDiv = {
+  margin: '20px auto',
+  width: '500px',
+  backgroundColor: 'white',
+  borderRadius: '20px',
+  padding: '40px 30px',
+};
+
+const header = {
+  textAlign: 'center' as 'center',
+  color: colorForest,
+};
+
+const content = {
+  padding: '35px',
+  textAlign: 'center' as 'center',
+};
+
+const footer = {
+  margin: '0 auto',
+  paddingBottom: '10px',
+  textAlign: 'center' as 'center',
+  fontSize: '14px',
+};
+
+export const EmailTemplate: FC<{ title: string }> = ({ title, children }) => (
+  <div style={container}>
+    <img
+      style={{ ...visdomLogo, marginTop: '20px' }}
+      src={`${baseUrl}/visdomlogo.png`}
+      alt="visdomlogo"
+    />
+    <div style={formDiv}>
+      <div style={header}>
+        <h2> {title} </h2>
+      </div>
+      <hr />
+      <div style={content}>{children}</div>
+    </div>
+    <div style={footer}>
+      <img
+        style={{ ...visdomLogo, marginBottom: '8px' }}
+        src={`${baseUrl}/visdomlogo.png`}
+        alt="visdomlogo"
+      />
+      <div>
+        <b>Contact the team</b>
+      </div>
+      <div style={{ marginBottom: '20px' }}>visdom@vincit.fi</div>
+    </div>
+  </div>
+);
