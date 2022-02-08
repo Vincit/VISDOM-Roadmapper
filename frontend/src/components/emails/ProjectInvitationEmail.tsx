5548b2cf369c440facb6d56bcfadfb58c3826a23
Patric Kangasmäki <patric.kangasmaki@vincit.fi>
2022-02-08 08:28:23 +0200

    Add view for project invitation email

---
+++ frontend/src/components/emails/ProjectInvitationEmail.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable no-template-curly-in-string */
+import { emailPage, EmailTemplate } from './EmailTemplate';
+import {
+  textStyle,
+  buttonStyle,
+  iconStyle,
+  subtitleStyle,
+} from './emailStyles';
+import { baseUrl, token } from './emailTestEnv';
+
+const title = 'You’ve been invited to a project';
+const projectName = '${projectName}';
+const roleType = '${roleType}';
+
+export const ProjectInvitationEmail = emailPage(title, () => (
+  <EmailTemplate title="Hi there,">
+    <div>
+      <img
+        style={iconStyle}
+        src={`${baseUrl}/invitationicon.png`}
+        alt="lock-icon"
+      />
+    </div>
+    <h2 style={{ marginTop: '15px' }}>{title}</h2>
+    <div style={textStyle}>
+      <p style={subtitleStyle}>
+        You have been invited to join project <b>{projectName}</b> as{' '}
+        <b>{roleType}</b>.
+      </p>
+      <hr />
+      <p style={{ marginTop: '25px' }}>
+        To accept project invitation, log in or register to Visdom by clicking
+        the button below.
+      </p>
+      <a href={`${baseUrl}/join/${token}`} style={{ textDecoration: 'none' }}>
+        <div style={buttonStyle}>Join project</div>
+      </a>
+      <div>
+        <p>Or copy the link below to your browser:</p>
+        <p>
+          {baseUrl}/join/{token}
+        </p>
+      </div>
+    </div>
+    <div style={{ ...textStyle, marginTop: '40px', marginBottom: '-40px' }}>
+      <i>Don’t want to join the project? You can ignore this message.</i>
+    </div>
+  </EmailTemplate>
+));
