526ace9a66b20d7f6f46c72c53a6ea7fadb8618c
Juho Eerola <juho.eerola@vincit.fi>
2022-02-08 12:51:34 +0200

    Fix constraint on task table
    This adds the roadmapId to the constraint, so that the same task can be
    imported to multiple roadmaps.

---
+++ server/src/migrations/20220208124324_fixTaskImportConstraint.ts
@@ -0,0 +1,13 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.alterTable('tasks', (table) => {
+    table.dropUnique(['externalId', 'importedFrom']);
+    table.unique(['roadmapId', 'externalId', 'importedFrom']);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  // Do nothing, the previous constraint was incorrect,
+  // and as it was also stricter this might cause conflicts.
+}
