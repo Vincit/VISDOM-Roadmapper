1ad3a5604a9c691bff33ca57bcfbc050634a78cd
Aleksi Kytölä <aleksi.kytola@vincit.fi>
2022-02-09 09:55:43 +0200

    Add migration to rescale ratings from 0-10 to 0-5

---
+++ server/src/migrations/20220209093644_rescaleRatings.ts
@@ -0,0 +1,13 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  knex('taskratings').update({
+    value: knex.raw('floor((taskratings.value - 1) / 2) + 1'),
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  knex('taskratings').update({
+    value: knex.raw('taskratings.value * 2'),
+  });
+}
