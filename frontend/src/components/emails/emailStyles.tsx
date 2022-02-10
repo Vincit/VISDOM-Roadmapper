882c10b445af15a3ab2a87a900231d957b007742
Patric Kangasmäki <patric.kangasmaki@vincit.fi>
2022-02-10 10:01:13 +0200

    Create file for shared email inline styles

---
+++ frontend/src/components/emails/emailStyles.tsx
@@ -0,0 +1,44 @@
+export const colorBlack60 = '#6a6a6a';
+export const colorEmerald = '#0ec679';
+export const colorForest = '#289c6b';
+
+export const textStyle = {
+  color: colorBlack60,
+  fontSize: '14px',
+  lineHeight: '100%',
+  letterSpacing: '0.02em',
+};
+
+export const buttonStyle = {
+  margin: '20px 0',
+  width: '100%',
+  height: '50px',
+  border: 'none',
+  borderRadius: '30px',
+  backgroundColor: colorEmerald,
+  color: 'white',
+  fontWeight: 600,
+  fontSize: '16px',
+  lineHeight: '50px',
+  letterSpacing: '-0.02em',
+  cursor: 'pointer',
+  marginBottom: '20px',
+  textAlign: 'center' as 'center',
+};
+
+export const iconStyle = {
+  height: '56px',
+  width: 'auto',
+};
+
+export const subtitleStyle = {
+  fontFamily: 'Work Sans',
+  fontStyle: 'normal',
+  fontWeight: 600,
+  fontSize: '18px',
+  lineHeight: '100%',
+  marginTop: '30px',
+  marginBottom: '30px',
+};
+
+export const linkStyle = { textDecoration: 'none' };
