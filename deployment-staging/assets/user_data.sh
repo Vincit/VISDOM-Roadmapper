5a46e293d7d1d91f6a451ccf610b9e9be4d57bc2
Aleksi Kytölä <aleksi.kytola@vincit.fi>
2022-02-24 15:04:17 +0200

    Add CDK project for sandbox staging deployments

---
+++ deployment-staging/assets/user_data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# User data for when creating a new EC2 instance for NodeJS and CodeDeploy
+# Output userdata logs
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+
+# Download node & install with yum:
+curl --silent --location https://rpm.nodesource.com/setup_14.x | bash -
+yum -y install nodejs
+
