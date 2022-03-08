f6b3f74730346b1c0f8e714fdfde857c08315d8d
Iida Kainu <iida.kainu@vincit.fi>
2022-03-08 11:01:21 +0200

    Add virtualization to SortableTaskList

--- frontend/src/components/SortableTask.tsx
+++ frontend/src/components/SortableTask.tsx
@@ -1,16 +1,38 @@
-import { FC } from 'react';
-import { Draggable } from 'react-beautiful-dnd';
+import { CSSProperties, FC } from 'react';
+import { Draggable, DraggableProvided } from 'react-beautiful-dnd';
 import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
 import { Task } from '../redux/roadmaps/types';
 import { TaskRatingsText } from './TaskRatingsText';
 import css from './SortableTask.module.scss';
+
+export const StaticTask: FC<{
+  task: Task;
+  showRatings: boolean;
+  provided: DraggableProvided;
+  style?: CSSProperties;
+}> = ({ task, showRatings, provided, style }) => (
+  <div
+    className={css.taskDiv}
+    ref={provided.innerRef}
+    {...provided.draggableProps}
+    {...provided.dragHandleProps}
+    style={{ ...provided.draggableProps.style, ...(style && { ...style }) }}
+  >
+    <div className={css.leftSideDiv}>{task.name}</div>
+    <div className={css.rightSideDiv}>
+      {showRatings && <TaskRatingsText task={task} />}
+      <DragIndicatorIcon fontSize="small" />
+    </div>
+  </div>
+);
 
 export const SortableTask: FC<{
   task: Task;
   index: number;
   disableDragging: boolean;
   showRatings: boolean;
-}> = ({ task, index, disableDragging, showRatings }) => (
+  style?: CSSProperties;
+}> = ({ task, index, disableDragging, showRatings, style }) => (
   <Draggable
     key={task.id}
     draggableId={`${task.id}`}
@@ -18,18 +40,12 @@ export const SortableTask: FC<{
     isDragDisabled={disableDragging}
   >
     {(provided) => (
-      <div
-        className={css.taskDiv}
-        ref={provided.innerRef}
-        {...provided.draggableProps}
-        {...provided.dragHandleProps}
-      >
-        <div className={css.leftSideDiv}>{task.name}</div>
-        <div className={css.rightSideDiv}>
-          {showRatings && <TaskRatingsText task={task} />}
-          <DragIndicatorIcon fontSize="small" />
-        </div>
-      </div>
+      <StaticTask
+        task={task}
+        showRatings={showRatings}
+        provided={provided}
+        style={style}
+      />
     )}
   </Draggable>
 );
--- frontend/src/components/SortableTaskList.tsx
+++ frontend/src/components/SortableTaskList.tsx
@@ -1,13 +1,21 @@
-import { FC, useState } from 'react';
+import { CSSProperties, FC, useEffect, useRef, useState } from 'react';
+import { VariableSizeList } from 'react-window';
 import { useTranslation } from 'react-i18next';
 import { Droppable } from 'react-beautiful-dnd';
 import classNames from 'classnames';
+import AutoSizer from 'react-virtualized-auto-sizer';
 import { Task } from '../redux/roadmaps/types';
-import { SortableTask } from './SortableTask';
+import { SortableTask, StaticTask } from './SortableTask';
 import { SearchField } from './SearchField';
 import css from './SortableTaskList.module.scss';
 
 const classes = classNames.bind(css);
+
+type RowProps = {
+  data: Task[];
+  index: number;
+  style: CSSProperties;
+};
 
 export const SortableTaskList: FC<{
   listId: string;
@@ -28,6 +36,45 @@ export const SortableTaskList: FC<{
 }) => {
   const { t } = useTranslation();
   const [search, setSearch] = useState('');
+  const [searched, setSearched] = useState(tasks);
+  const listRef = useRef<VariableSizeList<any> | null>(null);
+  const [divRef, setDivRef] = useState<HTMLDivElement | null>(null);
+  const [rowHeights, setRowHeights] = useState<number[]>([]);
+
+  useEffect(() => {
+    setSearched(
+      tasks.filter(({ name }) => name.toLowerCase().includes(search)),
+    );
+  }, [search, tasks]);
+
+  useEffect(() => {
+    if (!divRef) return;
+    const heights = searched.map(({ name }) => {
+      divRef.textContent = name;
+      const textHeight = divRef.offsetHeight;
+      divRef.textContent = '';
+
+      return 22 + textHeight; // 22 = margin + padding
+    });
+    setRowHeights(heights);
+    listRef.current!.resetAfterIndex(0);
+  }, [searched, divRef]);
+
+  const Row = ({ data, index, style }: RowProps) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { height: _, ...rest } = style;
+    return (
+      <SortableTask
+        style={{ ...rest }}
+        key={data[index].id}
+        task={data[index]}
+        index={index}
+        disableDragging={disableDragging}
+        showRatings={!!showRatings}
+      />
+    );
+  };
+
   return (
     <div className={classes(css.sortableListWrapper, className)}>
       {showSearch && (
@@ -42,6 +89,14 @@ export const SortableTaskList: FC<{
         isDropDisabled={isDropDisabled}
         droppableId={listId}
         type="TASKS"
+        mode="virtual"
+        renderClone={(provided, _, rubric) => (
+          <StaticTask
+            task={tasks[rubric.source.index]}
+            showRatings={!!showRatings}
+            provided={provided}
+          />
+        )}
       >
         {(provided, snapshot) => (
           <div
@@ -52,18 +107,22 @@ export const SortableTaskList: FC<{
             ref={provided.innerRef}
             {...provided.droppableProps}
           >
-            {tasks
-              .filter(({ name }) => name.toLowerCase().includes(search))
-              .map((task, index) => (
-                <SortableTask
-                  key={task.id}
-                  task={task}
-                  index={index}
-                  disableDragging={disableDragging}
-                  showRatings={!!showRatings}
-                />
-              ))}
-            {provided.placeholder}
+            <AutoSizer>
+              {({ height, width }) => (
+                <VariableSizeList
+                  height={height}
+                  width={width}
+                  ref={listRef}
+                  itemCount={searched.length}
+                  itemSize={(idx) => rowHeights[idx] ?? 0}
+                  outerRef={provided.innerRef}
+                  itemData={searched}
+                >
+                  {Row}
+                </VariableSizeList>
+              )}
+            </AutoSizer>
+            <div ref={setDivRef} className={classes(css.measureTaskName)} />
           </div>
         )}
       </Droppable>
