fc44370eac801c05ef15dfc775234c2f5c108ee1
Iida Kainu <iida.kainu@vincit.fi>
2022-04-13 12:49:55 +0300

    Add delay to showing ExpandableColumn's children
    The expand-animation would cause some children's measuring logic to
    break. Instead, show the children after animation has ended.

--- frontend/src/components/ExpandableColumn.tsx
+++ frontend/src/components/ExpandableColumn.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode, CSSProperties } from 'react';
+import { FC, ReactNode, CSSProperties, useState, useEffect } from 'react';
 import classNames from 'classnames';
 import { ReactComponent as ExpandLess } from '../icons/expand_less.svg';
 import { ReactComponent as ExpandMore } from '../icons/expand_more.svg';
@@ -12,28 +12,42 @@ export const ExpandableColumn: FC<{
   title: ReactNode;
   className?: string;
   style?: CSSProperties;
-}> = ({ expanded, onToggle, title, children, className, style }) => (
-  <div className={className} style={style}>
-    <div
-      className={classes(css.expandableWrapper, {
-        [css.minimized]: !expanded,
-      })}
-    >
+}> = ({ expanded, onToggle, title, children, className, style }) => {
+  // delay showing children, to avoid breaking possible measuring divs
+  const [showChildren, setShowChildren] = useState(expanded);
+
+  useEffect(() => {
+    if (!expanded) {
+      setShowChildren(false);
+    } else {
+      const t = setTimeout(() => setShowChildren(true), 300);
+      return () => clearTimeout(t);
+    }
+  }, [expanded]);
+
+  return (
+    <div className={className} style={style}>
       <div
-        className={classes(css.expandableHeader, {
+        className={classes(css.expandableWrapper, {
           [css.minimized]: !expanded,
         })}
-        onClick={onToggle}
-        onKeyPress={onToggle}
-        role="button"
-        tabIndex={0}
       >
-        {expanded ? <ExpandLess /> : <ExpandMore />}
-        {title}
-      </div>
-      <div className={classes(css.expandableContent)}>
-        {expanded && children}
+        <div
+          className={classes(css.expandableHeader, {
+            [css.minimized]: !expanded,
+          })}
+          onClick={onToggle}
+          onKeyPress={onToggle}
+          role="button"
+          tabIndex={0}
+        >
+          {expanded ? <ExpandLess /> : <ExpandMore />}
+          {title}
+        </div>
+        {showChildren && (
+          <div className={classes(css.expandableContent)}>{children}</div>
+        )}
       </div>
     </div>
-  </div>
-);
+  );
+};
