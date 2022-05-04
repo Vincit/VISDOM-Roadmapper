cad776ad7a22a251fd821a308f9970f7455aeb20
AnttiVaarala <antti.vaarala@vincit.fi>
2022-05-04 14:34:23 +0300

    Fix conversion functions' limit

--- shared/utils/conversion.ts
+++ shared/utils/conversion.ts
@@ -2,7 +2,7 @@ export function convertScale(rating: number): number;
 export function convertScale(rating: number | undefined): number | undefined;
 export function convertScale(rating: number | undefined) {
   if (rating === undefined) return undefined;
-  else if (rating < 0 || rating > 5) return rating;
+  else if (rating < 0) return rating;
   return rating ** 2;
 }
 
@@ -10,6 +10,6 @@ export function revertScale(rating: number): number;
 export function revertScale(rating: number | undefined): number | undefined;
 export function revertScale(rating: number | undefined) {
   if (rating === undefined) return undefined;
-  else if (rating < 0 || rating > 25) return rating;
+  else if (rating < 0) return rating;
   return Math.sqrt(rating);
 }
