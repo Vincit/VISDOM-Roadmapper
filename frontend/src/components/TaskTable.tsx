1612c21913373ec6077704e48bcfdb646ffbed73
Juho Eerola <juho.eerola@vincit.fi>
2022-05-10 14:10:08 +0300

    Add option to show milestone names
    Removed virtualised list from the relation modal, since there really
    should not be that many (direct) relations for a single task.
    
    Same should probably be done for the relation table where this was
    copied from.

--- frontend/src/components/TaskTable.tsx
+++ frontend/src/components/TaskTable.tsx
@@ -1,13 +1,17 @@
 import { FC, CSSProperties, useRef, useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { skipToken } from '@reduxjs/toolkit/query/react';
 import { useHistory } from 'react-router-dom';
 import { Trans } from 'react-i18next';
 import { VariableSizeList } from 'react-window';
 import classNames from 'classnames';
 import DoneAllIcon from '@mui/icons-material/DoneAll';
+import { chosenRoadmapIdSelector } from '../redux/roadmaps/selectors';
 import { Task } from '../redux/roadmaps/types';
 import { TaskStatus } from '../../../shared/types/customTypes';
 import { paths } from '../routers/paths';
 import { TaskRatingsText } from './TaskRatingsText';
+import { apiV2 } from '../api/api';
 import css from './TaskTable.module.scss';
 
 const classes = classNames.bind(css);
@@ -17,9 +21,14 @@ export const TaskRow: FC<{
   style?: CSSProperties;
   largeIcons?: true;
   onClick?: () => unknown;
-}> = ({ task, style, largeIcons, onClick }) => {
+  showMilestoneName?: boolean;
+}> = ({ task, style, largeIcons, onClick, showMilestoneName }) => {
   const history = useHistory();
   const toTask = `${paths.roadmapHome}/${task.roadmapId}${paths.roadmapRelative.tasks}/${task.id}`;
+  const roadmapId = useSelector(chosenRoadmapIdSelector);
+  const { data: versions } = apiV2.useGetVersionsQuery(roadmapId ?? skipToken, {
+    skip: !showMilestoneName,
+  });
   return (
     <div
       role="button"
@@ -35,10 +44,20 @@ export const TaskRow: FC<{
       style={style}
       className={classes(css.task)}
     >
-      {task.status === TaskStatus.COMPLETED && (
-        <DoneAllIcon className={classes(css.doneIcon)} />
-      )}
-      <div className={classes(css.taskName)}>{task.name}</div>
+      <div className={classes(css.namesContainer)}>
+        <div>
+          {task.status === TaskStatus.COMPLETED && (
+            <DoneAllIcon className={classes(css.doneIcon)} />
+          )}
+          <div className={classes(css.taskName)}>{task.name}</div>
+        </div>
+        {showMilestoneName && (
+          <div className={classes(css.milestoneName)}>
+            {versions?.find((v) => v.tasks.some((t) => t.id === task.id))
+              ?.name ?? 'Unordered'}
+          </div>
+        )}
+      </div>
       <div className={classes(css.taskRatingTexts)}>
         <TaskRatingsText task={task} largeIcons={largeIcons} />
       </div>
