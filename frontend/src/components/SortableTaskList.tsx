11164c97b7c08a9afca181c1362d82f684be0c74
Jimi Rantanen <jimi.rantanen@vincit.fi>
2022-06-08 12:48:07 +0300

    Fix drag issue with nested scroll containers

--- frontend/src/components/SortableTaskList.tsx
+++ frontend/src/components/SortableTaskList.tsx
@@ -1,9 +1,15 @@
-import { CSSProperties, FC, useEffect, useState, useCallback } from 'react';
+import {
+  CSSProperties,
+  FC,
+  useEffect,
+  useState,
+  useCallback,
+  useRef,
+} from 'react';
 import { FixedSizeList } from 'react-window';
 import { useTranslation } from 'react-i18next';
 import { Droppable } from 'react-beautiful-dnd';
 import classNames from 'classnames';
-import AutoSizer from 'react-virtualized-auto-sizer';
 import { Task } from '../redux/roadmaps/types';
 import { SortableTask, StaticTask } from './SortableTask';
 import { SearchField } from './SearchField';
@@ -41,14 +47,20 @@ export const SortableTaskList: FC<{
   const { t } = useTranslation();
   const [search, setSearch] = useState('');
   const [searched, setSearched] = useState(tasks);
+  const [listHeight, setListHeight] = useState(0);
   const itemHeight = 52; // enough for two lines for the task name
   const itemMargin = 6;
+  const divRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     setSearched(
       tasks.filter(({ name }) => name.toLowerCase().includes(search)),
     );
   }, [search, tasks]);
+
+  useEffect(() => {
+    if (divRef.current) setListHeight(divRef.current.clientHeight);
+  }, []);
 
   const Row = useCallback(
     ({ data, index, style }: RowProps) => {
@@ -82,46 +94,44 @@ export const SortableTaskList: FC<{
           />
         </div>
       )}
-      <Droppable
-        isDropDisabled={isDropDisabled}
-        droppableId={listId}
-        type="TASKS"
-        mode="virtual"
-        renderClone={(provided, _, rubric) => (
-          <StaticTask
-            task={searched[rubric.source.index]}
-            showRatings={!!showRatings}
-            showInfoIcon={!!showInfoIcon}
-            provided={provided}
-          />
-        )}
-      >
-        {(provided, snapshot) => (
-          <div
-            className={classes(css.sortableList, {
-              [css.highlight]: snapshot.isDraggingOver,
-              [css.notAllowed]: isDropDisabled,
-            })}
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-          >
-            <AutoSizer>
-              {({ height, width }) => (
-                <FixedSizeList
-                  height={height}
-                  width={width}
-                  itemCount={searched.length}
-                  itemSize={itemHeight + itemMargin}
-                  outerRef={provided.innerRef}
-                  itemData={searched}
-                >
-                  {Row}
-                </FixedSizeList>
-              )}
-            </AutoSizer>
-          </div>
-        )}
-      </Droppable>
+      <div ref={divRef} className={classes(css.droppableContainer)}>
+        <Droppable
+          isDropDisabled={isDropDisabled}
+          droppableId={listId}
+          type="TASKS"
+          mode="virtual"
+          renderClone={(provided, _, rubric) => (
+            <StaticTask
+              task={searched[rubric.source.index]}
+              showRatings={!!showRatings}
+              showInfoIcon={!!showInfoIcon}
+              provided={provided}
+            />
+          )}
+        >
+          {(provided, snapshot) => (
+            <div
+              className={classes(css.sortableList, {
+                [css.highlight]: snapshot.isDraggingOver,
+                [css.notAllowed]: isDropDisabled,
+              })}
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              <FixedSizeList
+                height={listHeight}
+                width="100%"
+                itemCount={searched.length}
+                itemSize={itemHeight + itemMargin}
+                outerRef={provided.innerRef}
+                itemData={searched}
+              >
+                {Row}
+              </FixedSizeList>
+            </div>
+          )}
+        </Droppable>
+      </div>
     </div>
   );
 };
